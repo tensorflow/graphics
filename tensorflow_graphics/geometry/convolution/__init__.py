0ea6d8a3274f964828f44c9a298a97cb2b0c667b
Julien Valentin <julienvalentin@google.com>
2019-08-12 07:30:21 -0700

    Adding missing import in the __init__ of the convolution module.
    PiperOrigin-RevId: 262918864

--- tensorflow_graphics/geometry/convolution/__init__.py
+++ tensorflow_graphics/geometry/convolution/__init__.py
@@ -17,6 +17,7 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow_graphics.geometry.convolution import graph_convolution
+from tensorflow_graphics.geometry.convolution import graph_pooling
 from tensorflow_graphics.geometry.convolution import utils
 from tensorflow_graphics.util import export_api as _export_api
 
