a791941649a54e973afceeec60fce5c762c3e9bc
Tzu-Mao Li <tzumao@mit.edu>
2019-12-12 12:28:37 -0800

    change package name "pyredner_tensorflow" to "redner"

--- tensorflow_graphics/models/__init__.py
+++ tensorflow_graphics/models/__init__.py
@@ -1,5 +1,5 @@
 """Models module."""
-from tensorflow_graphics.models import pyredner_tensorflow
+from tensorflow_graphics.models import redner
 from tensorflow_graphics.util import export_api as _export_api
 
 # API contains submodules of tensorflow_graphics.models.
