bcfdac7d4d6bc43f971ce8cf53290bb1154471cf
Aviv Segall <segaviv@google.com>
2020-01-15 10:19:56 -0800

    Add a point light module with the implementation of the rendering equation for a point light.
    PiperOrigin-RevId: 289884944

---
+++ tensorflow_graphics/rendering/light/point_light.py
@@ -0,0 +1,185 @@
+#Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module implements the rendering equation for a point light."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+import tensorflow as tf
+
+from tensorflow_graphics.math import vector
+from tensorflow_graphics.util import asserts
+from tensorflow_graphics.util import export_api
+from tensorflow_graphics.util import shape
+
+
+def estimate_radiance(point_light_radiance,
+                      point_light_position,
+                      surface_point_position,
+                      surface_point_normal,
+                      observation_point,
+                      brdf,
+                      name=None,
+                      reflected_light_fall_off=False):
+  """Estimates the spectral radiance of a point light reflected from the surface point towards the observation point.
+
+  Note:
+    In the following, A1 to An are optional batch dimensions, which must be
+    broadcast compatible.
+    B1 to Bm are optional batch dimensions for the lights, which must be
+    broadcast compatible.
+
+  Note:
+    In case the light or the observation point are located behind the surface
+    the function will return 0.
+
+  Note:
+    The gradient of this function is not smooth when the dot product of the
+    normal with the light-to-surface or surface-to-observation vectors is 0.
+
+  Args:
+    point_light_radiance: A tensor of shape '[B1, ..., Bm, K]', where the last
+      axis represents the radiance of the point light at a specific wave length.
+    point_light_position: A tensor of shape `[B1, ..., Bm, 3]`, where the last
+      axis represents the position of the point light.
+    surface_point_position: A tensor of shape `[A1, ..., An, 3]`, where the last
+      axis represents the position of the surface point.
+    surface_point_normal: A tensor of shape `[A1, ..., An, 3]`, where the last
+      axis represents the normalized surface normal at the given surface point.
+    observation_point: A tensor of shape `[A1, ..., An, 3]`, where the last axis
+      represents the observation point.
+    brdf: The BRDF of the surface as a function of:
+      incoming_light_direction - The incoming light direction as the last axis
+      of a tensor with shape `[A1, ..., An, 3]`.
+      outgoing_light_direction - The outgoing light direction as the last axis
+      of a tensor with shape `[A1, ..., An, 3]`.
+      surface_point_normal - The surface normal as the last axis of a tensor
+      with shape `[A1, ..., An, 3]`.
+      Note - The BRDF should return a tensor of size '[A1, ..., An, K]' where
+      the last axis represents the amount of reflected light in each wave
+      length.
+    name: A name for this op. Defaults to "estimate_radiance".
+    reflected_light_fall_off: A boolean specifying whether or not to include the
+      fall off of the light reflected from the surface towards the observation
+      point in the calculation. Defaults to False.
+
+  Returns:
+    A tensor of shape `[A1, ..., An, B1, ..., Bm, K]`, where the last
+      axis represents the amount of light received at the observation point
+      after being reflected from the given surface point.
+
+  Raises:
+    ValueError: if the shape of `point_light_position`,
+    `surface_point_position`, `surface_point_normal`, or `observation_point` is
+    not supported.
+    InvalidArgumentError: if 'surface_point_normal' is not normalized.
+  """
+  with tf.compat.v1.name_scope(name, "estimate_radiance", [
+      point_light_radiance, point_light_position, surface_point_position,
+      surface_point_normal, observation_point, brdf
+  ]):
+    point_light_radiance = tf.convert_to_tensor(value=point_light_radiance)
+    point_light_position = tf.convert_to_tensor(value=point_light_position)
+    surface_point_position = tf.convert_to_tensor(value=surface_point_position)
+    surface_point_normal = tf.convert_to_tensor(value=surface_point_normal)
+    observation_point = tf.convert_to_tensor(value=observation_point)
+
+    shape.check_static(
+        tensor=point_light_position,
+        tensor_name="point_light_position",
+        has_dim_equals=(-1, 3))
+    shape.check_static(
+        tensor=surface_point_position,
+        tensor_name="surface_point_position",
+        has_dim_equals=(-1, 3))
+    shape.check_static(
+        tensor=surface_point_normal,
+        tensor_name="surface_point_normal",
+        has_dim_equals=(-1, 3))
+    shape.check_static(
+        tensor=observation_point,
+        tensor_name="observation_point",
+        has_dim_equals=(-1, 3))
+    shape.compare_batch_dimensions(
+        tensors=(surface_point_position, surface_point_normal,
+                 observation_point),
+        tensor_names=("surface_point_position", "surface_point_normal",
+                      "observation_point"),
+        last_axes=-2,
+        broadcast_compatible=True)
+    shape.compare_batch_dimensions(
+        tensors=(point_light_radiance, point_light_position),
+        tensor_names=("point_light_radiance", "point_light_position"),
+        last_axes=-2,
+        broadcast_compatible=True)
+    surface_point_normal = asserts.assert_normalized(surface_point_normal)
+
+    # Get the number of lights dimensions (B1,...,Bm).
+    lights_num_dimensions = max(
+        len(point_light_radiance.shape), len(point_light_position.shape)) - 1
+    # Reshape the other parameters so they can be broadcasted to the output of
+    # shape [A1,...,An, B1,...,Bm, K].
+    surface_point_position = tf.reshape(
+        surface_point_position,
+        surface_point_position.shape[:-1] + (1,) * lights_num_dimensions + (3,))
+    surface_point_normal = tf.reshape(
+        surface_point_normal,
+        surface_point_normal.shape[:-1] + (1,) * lights_num_dimensions + (3,))
+    observation_point = tf.reshape(
+        observation_point,
+        observation_point.shape[:-1] + (1,) * lights_num_dimensions + (3,))
+
+    light_to_surface_point = surface_point_position - point_light_position
+    distance_light_surface_point = tf.norm(
+        tensor=light_to_surface_point, axis=-1, keepdims=True)
+    incoming_light_direction = tf.math.l2_normalize(
+        light_to_surface_point, axis=-1)
+    surface_to_observation_point = observation_point - surface_point_position
+    outgoing_light_direction = tf.math.l2_normalize(
+        surface_to_observation_point, axis=-1)
+    brdf_value = brdf(incoming_light_direction, outgoing_light_direction,
+                      surface_point_normal)
+    incoming_light_dot_surface_normal = vector.dot(-incoming_light_direction,
+                                                   surface_point_normal)
+    outgoing_light_dot_surface_normal = vector.dot(outgoing_light_direction,
+                                                   surface_point_normal)
+
+    estimated_radiance = (point_light_radiance * \
+                          brdf_value * incoming_light_dot_surface_normal) / \
+        (4. * math.pi * tf.math.square(distance_light_surface_point))
+
+    if reflected_light_fall_off:
+      distance_surface_observation_point = tf.norm(
+          tensor=surface_to_observation_point, axis=-1, keepdims=True)
+      estimated_radiance = estimated_radiance / \
+          tf.math.square(distance_surface_observation_point)
+
+    # Create a condition for checking whether the light or observation point are
+    # behind the surface.
+    min_dot = tf.minimum(incoming_light_dot_surface_normal,
+                         outgoing_light_dot_surface_normal)
+    common_shape = shape.get_broadcasted_shape(min_dot.shape,
+                                               estimated_radiance.shape)
+    d_val = lambda dim: 1 if dim is None else tf.compat.v1.dimension_value(dim)
+    common_shape = [d_val(dim) for dim in common_shape]
+    condition = tf.broadcast_to(tf.greater_equal(min_dot, 0.0), common_shape)
+
+    return tf.compat.v1.where(condition, estimated_radiance,
+                              tf.zeros_like(estimated_radiance))
+
+
+# API contains all public functions and classes.
+__all__ = export_api.get_functions_and_classes()
---
+++ tensorflow_graphics/rendering/light/tests/point_light_test.py
@@ -0,0 +1,361 @@
+#Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for point light."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+
+from absl.testing import flagsaver
+from absl.testing import parameterized
+import numpy as np
+import tensorflow as tf
+
+from tensorflow_graphics.rendering.light import point_light
+from tensorflow_graphics.util import test_case
+
+
+def fake_brdf(incoming_light_direction, outgoing_light_direction,
+              surface_point_normal):
+  del incoming_light_direction, surface_point_normal
+  return outgoing_light_direction
+
+
+def returning_zeros_brdf(incoming_light_direction, outgoing_light_direction,
+                         surface_point_normal):
+  del incoming_light_direction, outgoing_light_direction
+  return tf.zeros_like(surface_point_normal)
+
+
+def random_tensor(tensor_shape):
+  return np.random.uniform(low=-100.0, high=100.0, size=tensor_shape)
+
+
+def random_estimate_radiance():
+  tensor_size = np.random.randint(10)
+  tensor_shape = np.random.randint(1, 10, size=(tensor_size)).tolist()
+  light_tensor_size = np.random.randint(10)
+  lights_tensor_shape = np.random.randint(
+      1, 10, size=(light_tensor_size)).tolist()
+  point_light_radiance_init = random_tensor(lights_tensor_shape + [1])
+  point_light_position_init = random_tensor(lights_tensor_shape + [3])
+  surface_point_position_init = random_tensor(tensor_shape + [3])
+  surface_point_normal_init = random_tensor(tensor_shape + [3])
+  observation_point_init = random_tensor(tensor_shape + [3])
+  point_light_radiance = tf.convert_to_tensor(value=point_light_radiance_init)
+  point_light_position = tf.convert_to_tensor(value=point_light_position_init)
+  surface_point_position = tf.convert_to_tensor(
+      value=surface_point_position_init)
+  surface_point_normal = tf.convert_to_tensor(value=surface_point_normal_init)
+  observation_point = tf.convert_to_tensor(value=observation_point_init)
+
+  estimated_radiance = point_light.estimate_radiance(
+      point_light_radiance, point_light_position, surface_point_position,
+      surface_point_normal, observation_point, fake_brdf)
+
+  return (point_light_radiance, point_light_radiance_init),\
+      (point_light_position, point_light_position_init),\
+      (surface_point_position, surface_point_position_init),\
+      (surface_point_normal, surface_point_normal_init),\
+      (observation_point, observation_point_init),\
+      estimated_radiance
+
+
+def preset_estimate_radiance():
+  delta = 1e-5
+  point_light_radiance_init = np.array(1.0).reshape((1, 1))
+  point_light_position_init = np.array((delta, 1.0, 0.0)).reshape((1, 3))
+  surface_point_position_init = np.array((0.0, 0.0, 0.0))
+  surface_point_normal_init = np.array((1.0, 0.0, 0.0))
+  observation_point_init = np.array((delta, 3.0, 0.0))
+  point_light_radiance = tf.convert_to_tensor(value=point_light_radiance_init)
+  point_light_position = tf.convert_to_tensor(value=point_light_position_init)
+  surface_point_position = tf.convert_to_tensor(
+      value=surface_point_position_init)
+  surface_point_normal = tf.convert_to_tensor(value=surface_point_normal_init)
+  observation_point = tf.convert_to_tensor(value=observation_point_init)
+
+  estimated_radiance = point_light.estimate_radiance(
+      point_light_radiance, point_light_position, surface_point_position,
+      surface_point_normal, observation_point, fake_brdf)
+
+  return (point_light_radiance, point_light_radiance_init),\
+      (point_light_position, point_light_position_init),\
+      (surface_point_position, surface_point_position_init),\
+      (surface_point_normal, surface_point_normal_init),\
+      (observation_point, observation_point_init),\
+      estimated_radiance
+
+
+class PointLightTest(test_case.TestCase):
+
+  @parameterized.parameters(
+      # Light direction is parallel to the surface normal.
+      ([1.], [[0., 0., 1.]], [2., 0., 0.], [1.0 / (4. * math.pi), 0., 0.]),
+      # Light direction is parallel to the surface normal and the reflected
+      # light fall off is included in the calculation.
+      ([1.], [[0., 0., 1.]], [2., 0., 0.], \
+       [0.25 / (4. * math.pi), 0., 0.], True),
+      # Light direction is perpendicular to the surface normal.
+      ([1.], [[3., 0., 0.]], [1., 2., 3.], [0., 0., 0.]),
+      # Angle between surface normal and the incoming light direction is pi/3.
+      ([1.], [[math.sqrt(3), 0., 1.]], \
+       [0., 1., 0.], [0., 0.125 / (4. * math.pi), 0.]),
+      # Angle between surface normal and the incoming light direction is pi/4.
+      ([1.], [[0., 1., 1.]], [1., 1., 0.],
+       [0.25 / (4. * math.pi), 0.25 / (4. * math.pi), 0.]),
+      # Light has 3 radiances.
+      ([2., 4., 1.], [[0., 1., 1.]], [1., 1., 0.],
+       [0.5 / (4. * math.pi), 1. / (4. * math.pi), 0.]),
+      # Light is behind the surface.
+      ([1.], [[0., 0., -2.]], [7., 0., 0.], [0., 0., 0.]),
+      # Observation point is behind the surface.
+      ([1.], [[0., 0., 2.]], [5., 0., -2.], [0., 0., 0.]),
+      # Light and observation point are behind the surface.
+      ([1.], [[0., 0., -2.]], [5., 0., -2.], [0., 0., 0.]),
+  )
+  def test_estimate_radiance_preset(self,
+                                    light_radiance,
+                                    light_pos,
+                                    observation_pos,
+                                    expected_result,
+                                    reflected_light_fall_off=False):
+    """Tests the output of estimate radiance function with various parameters.
+
+    In this test the point on the surface is always [0, 0, 0] ,the surface
+    normal is [0, 0, 1] and the fake brdf function returns the (normalized)
+    direction of the outgoing light as its output.
+
+    Args:
+     light_radiance: An array of size K representing the point light radiances.
+     light_pos: An array of size [3,] representing the point light positions.
+     observation_pos: An array of size [3,] representing the observation point.
+     expected_result: An array of size [3,] representing the expected result of
+       the estimated reflected radiance function.
+     reflected_light_fall_off: A boolean specifying whether or not to include
+       the fall off of the reflected light in the calculation. Defaults to
+       False.
+    """
+    tensor_size = np.random.randint(3) + 1
+    tensor_shape = np.random.randint(1, 10, size=(tensor_size)).tolist()
+    lights_tensor_size = np.random.randint(3) + 1
+    lights_tensor_shape = np.random.randint(
+        1, 10, size=(lights_tensor_size)).tolist()
+    point_light_radiance = np.tile(light_radiance, lights_tensor_shape + [1])
+    point_light_position = np.tile(light_pos, lights_tensor_shape + [1])
+    surface_point_normal = np.tile([0.0, 0.0, 1.0], tensor_shape + [1])
+    surface_point_position = np.tile([0.0, 0.0, 0.0], tensor_shape + [1])
+    observation_point = np.tile(observation_pos, tensor_shape + [1])
+    expected = np.tile(expected_result,
+                       tensor_shape + lights_tensor_shape + [1])
+
+    pred = point_light.estimate_radiance(
+        point_light_radiance,
+        point_light_position,
+        surface_point_position,
+        surface_point_normal,
+        observation_point,
+        fake_brdf,
+        reflected_light_fall_off=reflected_light_fall_off)
+
+    self.assertAllClose(expected, pred)
+
+  @flagsaver.flagsaver(tfg_add_asserts_to_graph=False)
+  def test_estimate_radiance_jacobian_wrt_point_light_position_random(self):
+    """Tests the Jacobian of the point lighting equation.
+
+    Verifies that the Jacobian of the point lighting equation with respect to
+    the point light position is correct for random inputs.
+    """
+    _, (point_light_position, point_light_position_init), _, _, _, \
+        estimated_radiance = random_estimate_radiance()
+
+    self.assert_jacobian_is_correct(point_light_position,
+                                    point_light_position_init,
+                                    estimated_radiance)
+
+  @flagsaver.flagsaver(tfg_add_asserts_to_graph=False)
+  def test_estimate_radiance_jacobian_wrt_surface_point_position_random(self):
+    """Tests the Jacobian of the point lighting equation.
+
+    Verifies that the Jacobian of the point lighting equation with respect to
+    the surface point position is correct for random inputs.
+    """
+    _, _, (surface_point_position, surface_point_position_init), _, _,\
+        estimated_radiance = random_estimate_radiance()
+
+    self.assert_jacobian_is_correct(surface_point_position,
+                                    surface_point_position_init,
+                                    estimated_radiance)
+
+  @flagsaver.flagsaver(tfg_add_asserts_to_graph=False)
+  def test_estimate_radiance_jacobian_wrt_surface_point_normal_random(self):
+    """Tests the Jacobian of the point lighting equation.
+
+    Verifies that the Jacobian of the point lighting equation with respect to
+    the surface point noraml is correct for random inputs.
+    """
+    _, _, _, (surface_point_normal, surface_point_normal_init), _,\
+        estimated_radiance = random_estimate_radiance()
+
+    self.assert_jacobian_is_correct(surface_point_normal,
+                                    surface_point_normal_init,
+                                    estimated_radiance)
+
+  @flagsaver.flagsaver(tfg_add_asserts_to_graph=False)
+  def test_estimate_radiance_jacobian_wrt_observation_point_random(self):
+    """Tests the Jacobian of the point lighting equation.
+
+    Verifies that the Jacobian of the point lighting equation with respect to
+    the observation point is correct for random inputs.
+    """
+    _, _, _, _, (observation_point, observation_point_init),\
+        estimated_radiance = random_estimate_radiance()
+
+    self.assert_jacobian_is_correct(observation_point, observation_point_init,
+                                    estimated_radiance)
+
+  @flagsaver.flagsaver(tfg_add_asserts_to_graph=False)
+  def test_estimate_radiance_jacobian_wrt_point_light_position_preset(self):
+    """Tests the Jacobian of the point lighting equation.
+
+    Verifies that the Jacobian of the point lighting equation with respect to
+    the point light position is correct when the light direction is orthogonal
+    to the surface normal.
+    """
+    _, (point_light_position, point_light_position_init), _, _, _, \
+        estimated_radiance = preset_estimate_radiance()
+
+    self.assert_jacobian_is_correct(point_light_position,
+                                    point_light_position_init,
+                                    estimated_radiance)
+
+  @flagsaver.flagsaver(tfg_add_asserts_to_graph=False)
+  def test_estimate_radiance_jacobian_wrt_surface_point_position_preset(self):
+    """Tests the Jacobian of the point lighting equation.
+
+    Verifies that the Jacobian of the point lighting equation with respect to
+    the surface point position is correct when the light direction is orthogonal
+    to the surface normal.
+    """
+    _, _, (surface_point_position, surface_point_position_init), _, _,\
+        estimated_radiance = preset_estimate_radiance()
+
+    self.assert_jacobian_is_correct(surface_point_position,
+                                    surface_point_position_init,
+                                    estimated_radiance)
+
+  @flagsaver.flagsaver(tfg_add_asserts_to_graph=False)
+  def test_estimate_radiance_jacobian_wrt_surface_point_normal_preset(self):
+    """Tests the Jacobian of the point lighting equation.
+
+    Verifies that the Jacobian of the point lighting equation with respect to
+    the surface point normal is correct when the light direction is orthogonal
+    to the surface normal.
+    """
+    _, _, _, (surface_point_normal, surface_point_normal_init), _,\
+        estimated_radiance = preset_estimate_radiance()
+
+    self.assert_jacobian_is_correct(surface_point_normal,
+                                    surface_point_normal_init,
+                                    estimated_radiance)
+
+  @flagsaver.flagsaver(tfg_add_asserts_to_graph=False)
+  def test_estimate_radiance_jacobian_wrt_observation_point_preset(self):
+    """Tests the Jacobian of the point lighting equation.
+
+    Verifies that the Jacobian of the point lighting equation with respect to
+    the observation point is correct when the light direction is orthogonal to
+    the surface normal.
+    """
+    _, _, _, _, (observation_point, observation_point_init),\
+        estimated_radiance = preset_estimate_radiance()
+
+    self.assert_jacobian_is_correct(observation_point, observation_point_init,
+                                    estimated_radiance)
+
+  @parameterized.parameters(
+      ((1, 1), (1, 3), (3,), (3,), (3,)),
+      ((4, 1, 1), (4, 1, 3), (1, 3), (1, 3), (1, 3)),
+      ((3, 2, 1), (3, 2, 3), (2, 3), (2, 3), (2, 3)),
+      ((1, 1), (3,), (1, 3), (1, 2, 3), (1, 3)),
+      ((4, 5, 1), (3, 4, 5, 3), (1, 3), (1, 2, 2, 3), (1, 2, 3)),
+      ((1,), (1, 2, 2, 3), (1, 2, 3), (1, 3), (3,)),
+  )
+  def test_estimate_radiance_shape_exception_not_raised(self, *shape):
+    """Tests that the shape exceptions are not raised."""
+    self.assert_exception_is_not_raised(
+        point_light.estimate_radiance, shape, brdf=returning_zeros_brdf)
+
+  @parameterized.parameters(
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 1), (3,), (3,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (5, 1), (5, 2), (3,), (3,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 4), (3,), (3,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (1,), (3,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (2,), (3,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (4,), (3,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (3,), (1,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (3,), (2,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (3,), (4,),
+       (3,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (3,), (3,),
+       (4,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (3,), (3,),
+       (2,)),
+      ("must have exactly 3 dimensions in axis -1", (1, 1), (1, 3), (3,), (3,),
+       (1,)),
+      ("Not all batch dimensions are broadcast-compatible.", (1, 3, 1),
+       (1, 3, 3), (2, 3), (4, 3), (3,)),
+      ("Not all batch dimensions are broadcast-compatible.", (1, 3, 1),
+       (1, 4, 3), (2, 3), (3,), (3,)),
+  )
+  def test_estimate_radiance_shape_exception_raised(self, error_msg, *shape):
+    """Tests that the shape exception is raised."""
+    self.assert_exception_is_raised(
+        point_light.estimate_radiance,
+        error_msg,
+        shape,
+        brdf=returning_zeros_brdf)
+
+  def test_estimate_radiance_value_exceptions_raised(self):
+    """Tests that the value exceptions are raised correctly."""
+    point_light_radiance = random_tensor(tensor_shape=(1, 1))
+    point_light_position = random_tensor(tensor_shape=(1, 3))
+    surface_point_position = random_tensor(tensor_shape=(3,))
+    surface_point_normal = random_tensor(tensor_shape=(3,))
+    observation_point = random_tensor(tensor_shape=(3,))
+
+    # Verify that an InvalidArgumentError is raised as the given
+    # surface_point_normal is not normalized.
+    with self.assertRaises(tf.errors.InvalidArgumentError):
+      self.evaluate(
+          point_light.estimate_radiance(point_light_radiance,
+                                        point_light_position,
+                                        surface_point_position,
+                                        surface_point_normal, observation_point,
+                                        returning_zeros_brdf))
+
+
+if __name__ == "__main__":
+  test_case.main()
