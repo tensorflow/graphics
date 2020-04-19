b76364458d473de15a90036b1f458ecc5b59156b
Andrea Tagliasacchi <atagliasacchi@google.com>
2020-04-19 02:54:07 +0000

    pylint

--- tensorflow_graphics/nn/layer/pointnet.py
+++ tensorflow_graphics/nn/layer/pointnet.py
@@ -11,28 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 Implementation of the PointNet networks from:
 
 @inproceedings{qi2017pointnet,
-  title={Pointnet: Deep learning on point sets for 3d classification and segmentation},
+  title={Pointnet: Deep learning on point sets
+         for3d classification and segmentation},
   author={Qi, Charles R and Su, Hao and Mo, Kaichun and Guibas, Leonidas J},
-  booktitle={Proceedings of the IEEE conference on computer vision and pattern recognition},
+  booktitle={Proceedings of the IEEE conference on computer vision and pattern
+             recognition},
   pages={652--660},
-  year={2017}
-}
+  year={2017}}
 
-NOTE: scheduling of batchnorm momentum currently not available in keras. However, experimentally, using the batch norm from Keras resulted in better test accuracy (+1.5%) than the author's custom batch norm version
-(https://github.com/charlesq34/pointnet/blob/master/utils/tf_util.py), even when coupled with batch-norm momentum decay. Further, note the author's version is actually performing a "global normalization", as mentioned in the tf.nn.moments documentation (https://www.tensorflow.org/api_docs/python/tf/nn/moments).
+NOTE: scheduling of batchnorm momentum currently not available in keras. However
+experimentally, using the batch norm from Keras resulted in better test accuracy
+(+1.5%) than the author's [custom batch norm version](https://github.com/charlesq34/pointnet/blob/master/utils/tf_util.py)
+even when coupled with batchnorm momentum decay. Further, note the author's
+version is actually performing a "global normalization", as mentioned in the
+[tf.nn.moments documentation] (https://www.tensorflow.org/api_docs/python/tf/nn/moments).
 """
 
 import tensorflow as tf
-from tensorflow.keras import models  # TODO remove
-from tensorflow.keras import layers
+from tensorflow.keras.layers import Layer
 
 
-class PointNetConv2Layer(tf.keras.layers.Layer):
+class PointNetConv2Layer(Layer):
   def __init__(self, channels, momentum):
     super(PointNetConv2Layer, self).__init__()
     self.momentum = momentum
@@ -46,7 +49,7 @@ class PointNetConv2Layer(tf.keras.layers.Layer):
     return tf.nn.relu(self.bn(self.conv(x), training))
 
 
-class PointNetDenseLayer(tf.keras.layers.Layer):
+class PointNetDenseLayer(Layer):
   def __init__(self, channels, momentum):
     super(PointNetDenseLayer, self).__init__()
     self.momentum = momentum
@@ -60,7 +63,7 @@ class PointNetDenseLayer(tf.keras.layers.Layer):
     return tf.nn.relu(self.bn(self.dense(x), training))
 
 
-class VanillaEncoder(tf.keras.layers.Layer):
+class VanillaEncoder(Layer):
   def __init__(self, momentum=.5):
     super(VanillaEncoder, self).__init__()
     self.conv1 = PointNetConv2Layer(64, momentum)
@@ -114,15 +117,3 @@ class PointNetVanillaClassifier(tf.keras.layers.Layer):
     residual = cross_entropy(labels=labels, logits=logits)
     loss = tf.reduce_mean(residual)
     return loss
-
-if __name__ == "__main__":
-  # x = tf.random.uniform((32,2048,3))
-  # encoder = VanillaEncoder(2048, momentum=.5)
-  # print(encoder(x).shape)
-
-  x = tf.random.uniform((32, 2048, 1, 3))
-  layer = PointNetConv2Layer(64, .5)
-  # layer.build(x.shape)
-  y = layer(x)
-  print(y.shape)
-  # print([var.name for var in layer.trainable_variables])
