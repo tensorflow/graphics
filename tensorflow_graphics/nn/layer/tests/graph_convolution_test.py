b825ad5ce2cace93de172689b0aa1807cfadec53
Andrea <ataiya@users.noreply.github.com>
2020-04-25 02:26:55 -0400

    PointNet 1.0 implementation. (#248)

--- tensorflow_graphics/nn/layer/tests/graph_convolution_test.py
+++ tensorflow_graphics/nn/layer/tests/graph_convolution_test.py
@@ -45,6 +45,7 @@ def _dummy_data(batch_size, num_vertices, num_channels):
   return data, neighbors
 
 
+# pylint: disable=missing-class-docstring
 class GraphConvolutionTestFeatureSteeredConvolutionLayerTests(
     test_case.TestCase):
 
