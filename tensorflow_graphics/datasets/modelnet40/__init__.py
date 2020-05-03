2d62632ccbd9d6f46b8760c4e0d5080a8fed1ad6
Dominic Jack <thedomjack@gmail.com>
2020-05-04 01:32:44 +1000

    refactored configs, added meshes

--- tensorflow_graphics/datasets/modelnet40/__init__.py
+++ tensorflow_graphics/datasets/modelnet40/__init__.py
@@ -14,14 +14,17 @@
 # Lint as: python3
 """ModelNet40 classification dataset fom https://modelnet.cs.princeton.edu."""
 
+import abc
 import os
+import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from tensorflow_graphics.datasets.modelnet40 import off
 
 # Constants
-_URL = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+_POINTNET_URL = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
 
-_CITATION = """
+_POINTNET_CITATION = """
 @inproceedings{qi2017pointnet,
   title={Pointnet: Deep learning on point sets for 3d classification and segmentation},
   author={Qi, Charles R and Su, Hao and Mo, Kaichun and Guibas, Leonidas J},
@@ -31,7 +34,7 @@ _CITATION = """
 }
 """
 
-_DESCRIPTION = """
+_POINTNET_DESCRIPTION = """
 The dataset contains point clouds sampling CAD models from 40 different categories.
 The files have been retrieved from https://modelnet.cs.princeton.edu
 
@@ -53,36 +56,58 @@ _LABELS = [
 _CHECKSUM_DIR = os.path.join(os.path.dirname(__file__), 'checksums/')
 _CHECKSUM_DIR = os.path.normpath(_CHECKSUM_DIR)
 # BEGIN GOOGLE-INTERNAL
-_CHECKSUM_DIR = 'google3/third_party/py/tensorflow_graphics/datasets/modelnet40/checksums'
+# _CHECKSUM_DIR = 'google3/third_party/py/tensorflow_graphics/datasets/modelnet40/checksums'
 # END GOOGLE-INTERNAL
 tfds.download.add_checksums_dir(_CHECKSUM_DIR)
 
 
-class ModelNet40(tfds.core.GeneratorBasedBuilder):
-  """ModelNet40."""
+class ModelNet40Config(tfds.core.BuilderConfig):
+  """
+  Base class for ModelNet40 BuilderConfigs.
 
-  VERSION = tfds.core.Version('1.0.0')
+  The builder delegates required implementations to the builder config. This
+  allows multiple versions of the same dataset - including those that source
+  data from different locations e.g. pointnet - to exist under the same
+  tfds name.
+  """
+  @abc.abstractmethod
+  def info(self, builder):
+    """Delegated GeneratorBaseBuilder._info"""
+    raise NotImplementedError('Abstract method')
 
-  @staticmethod
-  def load(*args, **kwargs):
-    return tfds.load('model_net40', *args, **kwargs)
+  @abc.abstractmethod
+  def split_generators(self, download_manager):
+    """Delegated GeneratorBaseBuilder._split_generators"""
+    raise NotImplementedError('Abstract method')
 
-  def _info(self):
+  @abc.abstractmethod
+  def generate_examples(self, **kwargs):
+    """Delegated GeneratorBaseBuilder._generate_examples"""
+    raise NotImplementedError('Abstract method')
+
+
+class PointnetConfig(ModelNet40Config):
+  """Config for point cloud data used in Pointnet."""
+  def __init__(self):
+    super().__init__(
+        name='pointnet',
+        description=_POINTNET_DESCRIPTION,
+        version=tfds.core.Version('1.0.0')
+    )
+
+  def info(self, builder):
     return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
+        builder=builder,
         features=tfds.features.FeaturesDict({
             'points': tfds.features.Tensor(shape=(2048, 3), dtype=tf.float32),
             'label': tfds.features.ClassLabel(names=_LABELS)
         }),
         supervised_keys=('points', 'label'),
         homepage='https://modelnet.cs.princeton.edu',
-        citation=_CITATION,
+        citation=_POINTNET_CITATION,
     )
 
-  def _split_generators(self, download_manager):
-    """Returns SplitGenerators."""
-
+  def split_generators(self, download_manager):
     extracted_path = download_manager.download_and_extract(_URL)
 
     # Note: VALIDATION split was not provided by the authors
@@ -101,9 +126,8 @@ class ModelNet40(tfds.core.GeneratorBasedBuilder):
                     'modelnet40_ply_hdf5_2048/test_files.txt'),)),
     ]
 
-  def _generate_examples(self, filename_list_path):
-    """Yields examples."""
-
+  def generate_examples(self, filename_list_path):  # pylint:disable=arguments-differ
+    """Delegated GeneratorBaseBuilder._generate_examples"""
     ancestor_path = os.path.dirname(os.path.dirname(filename_list_path))
     with tf.io.gfile.GFile(filename_list_path, 'r') as fid:
       filename_list = fid.readlines()
@@ -124,3 +148,154 @@ class ModelNet40(tfds.core.GeneratorBasedBuilder):
               'points': points[imodel, :, :],
               'label': int(label[imodel])
           }
+
+
+_POLYMESH_DESCRIPTION = """\
+The dataset contains polygonal mesh data from 40 different categories.
+
+The files have been retrieved from https://modelnet.cs.princeton.edu
+"""
+
+_TRIMESH_DESCRIPTION = """\
+The dataset contains triangular mesh data from 40 different categories.
+
+The files have been retrieved from https://modelnet.cs.princeton.edu
+"""
+
+_MODELNET_CITATION = """\
+@inproceedings{wu20153d,
+  title={3d shapenets: A deep representation for volumetric shapes},
+  author={Wu, Zhirong and Song, Shuran and Khosla, Aditya and Yu, Fisher and Zhang, Linguang and Tang, Xiaoou and Xiao, Jianxiong},
+  booktitle={Proceedings of the IEEE conference on computer vision and pattern recognition},
+  pages={1912--1920},
+  year={2015}
+}
+"""
+
+class MeshConfig(ModelNet40Config):
+  """Base class for derivatives of the original .off mesh data."""
+  def __init__(self, input_key, input_feature, **kwargs):
+    self._input_key = input_key
+    self._input_feature = input_feature
+    super().__init__(**kwargs)
+
+  @abc.abstractmethod
+  def load(self, fp):
+    raise NotImplementedError('Abstract method')
+
+  def info(self, builder):
+    return tfds.core.DatasetInfo(
+        builder=builder,
+        features=tfds.features.FeaturesDict({
+            self._input_key: self._input_feature,
+            'example_index': tfds.features.Tensor(shape=(), dtype=tf.int64),
+            'label': tfds.features.ClassLabel(names=_LABELS)
+        }),
+        supervised_keys=(self._input_key, 'label'),
+        homepage='https://modelnet.cs.princeton.edu',
+        citation=_MODELNET_CITATION,
+    )
+
+  def split_generators(self, download_manager):
+    archive_res = download_manager.download(
+        'http://modelnet.cs.princeton.edu/ModelNet40.zip')
+    archive_iter = download_manager.iter_archive(archive_res)
+    return [
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs=dict(archive_iter=archive_iter, split_name='train'),
+        ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            gen_kwargs=dict(archive_iter=archive_iter, split_name='test'),
+        )
+    ]
+
+  def generate_examples(self, archive_iter, split_name):  # pylint:disable=arguments-differ
+    input_key = self._input_key
+
+    for path, fp in archive_iter:
+      if not path.endswith('.off'):
+        continue
+      class_name, split, fn = path.split('/')[-3:]
+      if split != split_name:
+        continue
+
+      inputs = self.load(fp)
+      if inputs is not None:
+        yield path, {
+            input_key: inputs,
+            'example_index': int(fn.split('_')[-1][:-4]) - 1,
+            'label': class_name,
+        }
+
+class PolymeshConfig(MeshConfig):
+  """MeshConfig with original polygonal meshes."""
+  def __init__(self):
+    super().__init__(
+        input_key='polymesh',
+        input_feature=dict(
+            vertices=tfds.features.Tensor(shape=(None, 3), dtype=tf.float32),
+            face_values=tfds.features.Tensor(shape=(None,), dtype=tf.int64),
+            face_lengths=tfds.features.Tensor(shape=(None,), dtype=tf.int64),
+        ),
+        name='polymesh',
+        version=tfds.core.Version('1.0.0'),
+        description=_POLYMESH_DESCRIPTION
+    )
+
+  def load(self, fp):  # pylint: disable=unused-argument
+    data = off.OffObject.from_file(fp)
+    return dict(
+        vertices=data.vertices.astype(np.float32),
+        face_values=data.face_values,
+        face_lengths=data.face_lengths,
+    )
+
+
+class TrimeshConfig(MeshConfig):
+  """MeshConfig with triangular mesh features."""
+  def __init__(self):
+    super().__init__(
+        input_key='trimesh',
+        input_feature=dict(
+            vertices=tfds.features.Tensor(shape=(None, 3), dtype=tf.float32),
+            faces=tfds.features.Tensor(shape=(None, 3), dtype=tf.int64),
+        ),
+        name='trimesh',
+        version=tfds.core.Version('1.0.0'),
+        description=_TRIMESH_DESCRIPTION,
+    )
+
+  def load(self, fp):
+    data = off.OffObject.from_file(fp)
+    return dict(
+        vertices=data.vertices.astype(np.float32),
+        faces=off.triangulated_faces(data.face_values, data.face_lengths)
+    )
+
+
+POINTNET = PointnetConfig()
+
+POLYMESH = PolymeshConfig()
+
+TRIMESH = TrimeshConfig()
+
+class ModelNet40(tfds.core.GeneratorBasedBuilder):
+  """ModelNet40."""
+  BUILDER_CONFIGS = [POINTNET, POLYMESH, TRIMESH]
+
+  @staticmethod
+  def load(*args, **kwargs):
+    return tfds.load('model_net40', *args, **kwargs)
+
+  def _info(self):
+    return self.builder_config.info(self)
+
+  def _split_generators(self, download_manager):
+    """Returns SplitGenerators."""
+    return self.builder_config.split_generators(download_manager)
+
+  def _generate_examples(self, **kwargs):
+    """Yields examples."""
+    return self.builder_config.generate_examples(**kwargs)
---
+++ tensorflow_graphics/datasets/modelnet40/off.py
@@ -0,0 +1,194 @@
+"""Provides `OffObject` for interfacing with `.off` files."""
+import numpy as np
+
+
+def _num_edges(face_lengths):
+  return sum(face_lengths)
+
+
+def _parse_off_vertex(line):
+  return tuple(float(s) for s in line.split(' '))
+
+
+def _parse_off_face(line):
+  return tuple(int(s) for s in line.split(' ')[1:])
+
+
+class OffObject(object):
+  """Class for loading/saving `.off` files.
+
+  See e.g.
+  http://shape.cs.princeton.edu/benchmark/documentation/off_format.html
+
+  General polygonal meshes (a generalization of triangular meshes, where each
+  face can have any number of sides) can be expressed using a (num_vertices, 3)
+  float array of vertex coordinates and a jagged array of indices. i.e.
+
+  ```python
+  jagged_faces = [
+      [0, 1, 2, 3],
+      [2, 3, 4]
+    ]
+
+  ```
+  could represent a square with vertices indexed by [0, 1, 2, 3] and another
+  triangle with vertices indexed by [2, 3, 4]. Given some 3D vertex coordinate,
+  we could get the vertices of the square and triangle as follows.
+  ```
+  all_vertices = np.random.normal((5, 3))
+  square_vertices = all_vertices[jagged_faces[0]]
+  tri_vertices = all_vertices[jagged_faces[1]]
+  ```
+
+  While nice to think about, manipulating `jagged_arrays` using numpy slicing
+  is difficult. Instead, we store the values in `jagged_faces` and the number
+  of vertices in each face separately. The above `jagged_faces` would thus be
+  ```python
+  face_values = [0, 1, 2, 3, 2, 3, 4]
+  face_lengths = [4, 3]
+  ```
+
+  These are what is parsed/stored in `OffObject` instances.
+
+  ```python
+  with open('my_off_file.off', 'rb') as fp:
+    off_obj = OffObject.from_file(fp)
+  print(off_obj.face_values)  # [0, 1, 2, 3, 2, 3, 4]
+  print(off_obj.face_lengths) # [4, 3]
+  print(off_obj.vertices)     # (5, 3) float array
+  ```
+
+  This is consistent with tensorflows `tf.RaggedTensor.from_row_lengths`
+
+  See also `tensorflow_datasets.volume.utils.tri.trinagulated_faces` to convert
+  to a triangular mesh.
+  """
+
+  def __init__(self, vertices, face_values, face_lengths, num_edges=None):
+    self._num_edges = num_edges
+    self._vertices = np.asarray(vertices)
+    self._face_values = np.asarray(face_values)
+    self._face_lengths = np.asarray(face_lengths)
+
+  @property
+  def vertices(self):
+    return self._vertices
+
+  @property
+  def face_values(self):
+    return self._face_values
+
+  @property
+  def face_lengths(self):
+    return self._face_lengths
+
+  @property
+  def num_edges(self):
+    if self._num_edges is None:
+      self._num_edges = _num_edges(self._face_lengths)
+    return self._num_edges
+
+  @property
+  def num_vertices(self):
+    return len(self.vertices)
+
+  @property
+  def num_faces(self):
+    return len(self._face_values)
+
+  @staticmethod
+  def from_file(fp):
+    """Load OffObject from file."""
+    lines = fp.readlines()
+    lines = (l.decode("utf-8") for l in lines)
+    lines = (l.rstrip() for l in lines if not l.startswith('#'))
+    lines = (l for l in lines if l != '')
+    line_iter = iter(lines)
+    try:
+      line = next(line_iter)
+      if line[:3] != 'OFF':
+        raise IOError('Invalid .off file: must start with OFF')
+      line = line[3:]
+      if line == '':
+        line = next(line_iter)
+    except StopIteration:
+      raise IOError('Invalid off file - no header found.')
+
+    # not all num_edges are calculated - often just zero
+    # can be calculated from faces lazily
+    n_verts, num_faces, _ = (int(i) for i in line.split(' '))
+    try:
+      vertices = tuple(
+          _parse_off_vertex(next(line_iter)) for _ in range(n_verts))
+      faces = tuple(
+          _parse_off_face(next(line_iter)) for _ in range(num_faces))
+    except StopIteration:
+      raise IOError('Invalid off file - insufficient number of lines.')
+    try:
+      next(line_iter)
+      raise IOError('Invalid off file - too many lines')
+    except StopIteration:
+      face_values = np.concatenate(faces, axis=0)
+      face_lengths = [len(f) for f in faces]
+      return OffObject(vertices, face_values, face_lengths)
+
+  def to_file(self, fp):
+    """Dump object to file in .off format."""
+    fp.write(('OFF\n').encode('utf-8'))
+    fp.write(('%d %d %d\n' % (self.num_vertices, self.num_faces, self.num_edges)
+             ).encode('utf-8'))
+    for v in self.vertices:
+      fp.write(('%s\n' % ' '.join(str(vi) for vi in v)).encode('utf-8'))
+    values = self.face_values
+
+    for face_size in self.face_lengths:
+      face, values = np.split(values, (face_size,))  # pylint: disable=unbalanced-tuple-unpacking
+      fp.write(('%d %s\n' % (len(face), ' '.join(str(fi) for fi in face))
+               ).encode('utf-8'))
+
+
+def triangulated_faces(face_values, face_lengths):
+  """Triangulate the polygon mesh defined by face values/lengths.
+
+  face_values and face_lengths encode the jagged array defining the polygon
+  faces. For example, the mesh made up of a square with vertices [0, 1, 2, 3]
+  and a triangle with vertices indexed by [1, 2, 4] would be given by
+  ```python
+  face_values = [0, 1, 2, 3, 1, 2, 4]
+  face_lengths = [4, 3]
+  ```
+  i.e. the first 4 entries of `face_values` correspond to the square, and
+  `face_values[4:4+3]` correspond to the triangle. We triangulate this by
+  splitting the square into 2 triangles, [0, 1, 2] and [0, 2, 3] (note the
+  direction is preserved which is important if outwards direction is defined
+  by the ordering.
+  ```python
+  >>> triangulated_faces(face_values, face_lengths)
+  # [
+  #   [0, 1, 2],  # first half of square
+  #   [0, 2, 3],  # second half of square
+  #   [1, 2, 4],  # original triangle
+  # ]
+  ```
+
+  Args:
+    face_values: 1D int array of indices of vertices in a single lists
+    face_lengths: (num_poly_faces,) int array of number of indices in
+      `face_values` corresponding to each face.
+
+  Returns:
+    (num_tri_faces, 3) int array of vertex indices for each triangular face.
+  """
+  n = np.sum(face_lengths) - 2 * len(face_lengths)
+  out = np.empty((n, 3), dtype=face_values.dtype)
+  faces = np.split(face_values, np.cumsum(face_lengths[:-1]))
+  assert len(faces[-1]) == face_lengths[-1]
+
+  start = 0
+  for face in faces:
+    end = start + len(face) - 2
+    out[start:end, 0] = face[0]
+    out[start:end, 1] = face[1:-1]
+    out[start:end, 2] = face[2:]
+    start = end
+  return out
