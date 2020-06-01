3fabd621f29b48d49b9dc43aa015cfae8f48366f
Robin Baumann <mail@robin-baumann.com>
2020-06-01 12:44:01 +0200

    add Pix3D dataset template

--- tensorflow_graphics/datasets/pix3d/pix3d_test.py
+++ tensorflow_graphics/datasets/pix3d/pix3d_test.py
@@ -4,8 +4,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-
 import tensorflow_datasets.public_api as tfds
 from tensorflow_graphics.datasets import pix3d
 
@@ -14,19 +12,17 @@ class Pix3dTest(tfds.testing.DatasetBuilderTestCase):
   # TODO(pix3d):
   DATASET_CLASS = pix3d.Pix3d
   SPLITS = {
-      "train": 2,  # Number of fake train example
+      "train": 3,  # Number of fake train example
       "test": 1,  # Number of fake test example
   }
 
-  DL_EXTRACT_RESULT = ""
-  EXAMPLE_DIR = os.path.join(os.path.dirname(__file__), "fakes")
-  MOCK_OUT_FORBIDDEN_OS_FUNCTIONS = False
-
-  def setUp(self):
-    super(Pix3dTest, self).setUp()
-    self.builder._TRAIN_SPLIT_IDX = "fakes/pix3d_train.npy"
-    self.builder._TEST_SPLIT_IDX = "fakes/pix3d_test.npy"
+  # If you are calling `download/download_and_extract` with a dict, like:
+  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
+  # then the tests needs to provide the fake output paths relative to the
+  # fake data directory
+  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
 
 
 if __name__ == "__main__":
   tfds.testing.test_main()
+
