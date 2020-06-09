cad1b7319bafc67755a19da692ce164c4587459d
Robin Baumann <mail@robin-baumann.com>
2020-06-09 17:06:37 +0200

    add Pix3D DatasetInfo Spec

--- tensorflow_graphics/datasets/pix3d/pix3d.py
+++ tensorflow_graphics/datasets/pix3d/pix3d.py
@@ -4,9 +4,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow_datasets.public_api as tfds
+import tensorflow.compat.v2 as tf
 
-# TODO(pix3d): BibTeX citation
+import tensorflow_datasets.public_api as tfds
+from tensorflow_datasets import features as tfds_features
+from tensorflow_graphics.datasets import features as tfg_features
+
 _CITATION = """@inproceedings{pix3d,
   title={Pix3D: Dataset and Methods for Single-Image 3D Shape Modeling},
   author={Sun, Xingyuan and Wu, Jiajun and Zhang, Xiuming and Zhang, Zhoutong and Zhang, Chengkai and Xue, Tianfan and Tenenbaum, Joshua B and Freeman, William T},
@@ -15,7 +18,6 @@ _CITATION = """@inproceedings{pix3d,
 }
 """
 
-# TODO(pix3d):
 _DESCRIPTION = """Pix3D is a large-scale dataset of diverse image-shape pairs with pixel-level 2D-3D alignment. 
 It has wide applications in shape-related tasks including reconstruction, retrieval, viewpoint estimation, etc.
 
@@ -26,7 +28,7 @@ bounding-box, segmentation mask, intrinsic and extrinsic camera parameters and 2
 
 
 class Pix3d(tfds.core.GeneratorBasedBuilder):
-  """TODO(pix3d): Short description of my dataset."""
+  """Pix3D is a large-scale dataset of diverse image-shape pairs with pixel-level 2D-3D alignment."""
 
   # TODO(pix3d): Set up version.
   VERSION = tfds.core.Version('0.1.0')
@@ -39,14 +41,29 @@ class Pix3d(tfds.core.GeneratorBasedBuilder):
         description=_DESCRIPTION,
         # tfds.features.FeatureConnectors
         features=tfds.features.FeaturesDict({
-            # These are the features of your dataset like images, labels ...
+          'image': tfds_features.Image(shape=(None, None, 3), dtype=tf.uint8),
+          'image/filename': tfds_features.Text(),
+          'image/source': tfds_features.Text(),
+          '2d_keypoints': tfds_features.Tensor(shape=(None, None, 2), dtype=tf.float32),
+          'mask': tfds_features.Image(shape=(None, None, 1), dtype=tf.float32),
+          'model': tfg_features.TriangleMesh(),
+          'model/source': tfds_features.Text(),
+          '3d_keypoints': tfds_features.Tensor(shape=(None, 3), dtype=tf.float32),
+          'voxel': tfg_features.VoxelGrid(shape=(128, 128, 128)),
+          'pose': tfg_features.Pose(),
+          'camera': tfg_features.Camera(),
+          'category': tfds_features.ClassLabel(shape=(), dtype=tf.int64, num_classes=9),
+          'bbox': tfds_features.BBoxFeature(),
+          'truncated': tf.bool,
+          'occluded': tf.bool,
+          'slightly_occluded': tf.bool
         }),
         # If there's a common (input, target) tuple from the features,
         # specify them here. They'll be used if as_supervised=True in
         # builder.as_dataset.
         supervised_keys=(),
         # Homepage of the dataset for documentation
-        homepage='https://dataset-homepage/',
+        homepage='http://pix3d.csail.mit.edu/',
         citation=_CITATION,
     )
 
@@ -55,6 +72,7 @@ class Pix3d(tfds.core.GeneratorBasedBuilder):
     # TODO(pix3d): Downloads the data and defines the splits
     # dl_manager is a tfds.download.DownloadManager that can be used to
     # download and extract URLs
+
     return [
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
