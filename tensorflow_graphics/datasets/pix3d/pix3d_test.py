f0420bfb3d021e364370b6e552d9d60c05f73bf2
Robin Baumann <mail@robin-baumann.com>
2020-06-13 13:56:00 +0200

    Add fake data and test case for Pix3D

--- tensorflow_graphics/datasets/pix3d/pix3d_test.py
+++ tensorflow_graphics/datasets/pix3d/pix3d_test.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+
 import tensorflow_datasets.public_api as tfds
 from tensorflow_graphics.datasets import pix3d
 
@@ -12,17 +14,18 @@ class Pix3dTest(tfds.testing.DatasetBuilderTestCase):
   # TODO(pix3d):
   DATASET_CLASS = pix3d.Pix3d
   SPLITS = {
-      "train": 3,  # Number of fake train example
+      "train": 2,  # Number of fake train example
       "test": 1,  # Number of fake test example
   }
-
   # If you are calling `download/download_and_extract` with a dict, like:
   #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
   # then the tests needs to provide the fake output paths relative to the
   # fake data directory
   # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
 
+  DL_EXTRACT_RESULT = ""
+  EXAMPLE_DIR = os.path.join(os.path.dirname(__file__), "fakes")
+
 
 if __name__ == "__main__":
   tfds.testing.test_main()
-
