9db8e5582eb78af2b459ba8c7d1fa55f5178a9e8
Robin Baumann <mail@robin-baumann.com>
2020-07-06 22:10:28 +0200

    Fix Linting

--- tensorflow_graphics/datasets/pix3d/pix3d.py
+++ tensorflow_graphics/datasets/pix3d/pix3d.py
@@ -14,7 +14,8 @@ from tensorflow_datasets import features as tfds_features
 
 from tensorflow_graphics.datasets import features as tfg_features
 
-_CITATION = """@inproceedings{pix3d,
+_CITATION = """
+@inproceedings{pix3d,
   title={Pix3D: Dataset and Methods for Single-Image 3D Shape Modeling},
   author={Sun, Xingyuan and Wu, Jiajun and Zhang, Xiuming and Zhang, Zhoutong
   and Zhang, Chengkai and Xue, Tianfan and Tenenbaum, Joshua B and
@@ -24,7 +25,8 @@ _CITATION = """@inproceedings{pix3d,
 }
 """
 
-_DESCRIPTION = """Pix3D is a large-scale dataset of diverse image-shape pairs 
+_DESCRIPTION = """
+Pix3D is a large-scale dataset of diverse image-shape pairs
 with pixel-level 2D-3D alignment. It has wide applications in shape-related 
 tasks including reconstruction, retrieval, viewpoint estimation, etc.
 
@@ -48,7 +50,8 @@ Note:
 """
 
 _CHECKSUMS_DIR = os.path.normpath(
-  os.path.join(os.path.dirname(__file__), 'checksums/'))
+    os.path.join(os.path.dirname(__file__), 'checksums/')
+)
 tfds.download.add_checksums_dir(_CHECKSUMS_DIR)
 
 
@@ -68,69 +71,72 @@ class Pix3d(tfds.core.GeneratorBasedBuilder):
 
   def _info(self):
     return tfds.core.DatasetInfo(
-      builder=self,
-      # This is the description that will appear on the datasets page.
-      description=_DESCRIPTION,
-      # tfds.features.FeatureConnectors
-      features=tfds.features.FeaturesDict({
-        'image': tfds_features.Image(shape=(None, None, 3), dtype=tf.uint8),
-        'image/filename': tfds_features.Text(),
-        'image/source': tfds_features.Text(),
-        '2d_keypoints': tfds_features.FeaturesDict({
-          'num_annotators': tf.uint8,
-          'num_keypoints': tf.uint8,
-          'keypoints': tfds_features.Tensor(shape=(None,),
-                                            dtype=tf.float32),
+        builder=self,
+        # This is the description that will appear on the datasets page.
+        description=_DESCRIPTION,
+        # tfds.features.FeatureConnectors
+        features=tfds.features.FeaturesDict({
+            'image': tfds_features.Image(shape=(None, None, 3), dtype=tf.uint8),
+            'image/filename': tfds_features.Text(),
+            'image/source': tfds_features.Text(),
+            '2d_keypoints': tfds_features.FeaturesDict({
+                'num_annotators': tf.uint8,
+                'num_keypoints': tf.uint8,
+                'keypoints': tfds_features.Tensor(shape=(None,),
+                                                  dtype=tf.float32),
+            }),
+            'mask': tfds_features.Image(shape=(None, None, 1), dtype=tf.uint8),
+            'model': tfg_features.TriangleMesh(),
+            'model/source': tfds_features.Text(),
+            '3d_keypoints': tfds_features.Tensor(shape=(None, 3),
+                                                 dtype=tf.float32),
+            'voxel': tfg_features.VoxelGrid(shape=(128, 128, 128)),
+            'pose': tfg_features.Pose(),
+            'camera': tfds_features.FeaturesDict({
+                'parameters': tfg_features.Camera(),
+                'position_with_respect_to_object': tfds_features.Tensor(
+                    shape=(3,), dtype=tf.float32
+                ),
+                'inplane_rotation': tf.float32,
+            }),
+            'category': tfds_features.ClassLabel(
+                num_classes=len(self.CLASS_INDEX)
+            ),
+            'bbox': tfds_features.BBoxFeature(),
+            'truncated': tf.bool,
+            'occluded': tf.bool,
+            'slightly_occluded': tf.bool,
         }),
-        'mask': tfds_features.Image(shape=(None, None, 1), dtype=tf.uint8),
-        'model': tfg_features.TriangleMesh(),
-        'model/source': tfds_features.Text(),
-        '3d_keypoints': tfds_features.Tensor(shape=(None, 3),
-                                             dtype=tf.float32),
-        'voxel': tfg_features.VoxelGrid(shape=(128, 128, 128)),
-        'pose': tfg_features.Pose(),
-        'camera': tfds_features.FeaturesDict({
-          'parameters': tfg_features.Camera(),
-          'position_with_respect_to_object': tfds_features.Tensor(
-            shape=(3,), dtype=tf.float32),
-          'inplane_rotation': tf.float32,
-        }),
-        'category': tfds_features.ClassLabel(num_classes=len(self.CLASS_INDEX)),
-        'bbox': tfds_features.BBoxFeature(),
-        'truncated': tf.bool,
-        'occluded': tf.bool,
-        'slightly_occluded': tf.bool
-      }),
-      # If there's a common (input, target) tuple from the features,
-      # specify them here. They'll be used if as_supervised=True in
-      # builder.as_dataset.
-      supervised_keys=None,
-      # Homepage of the dataset for documentation
-      homepage='http://pix3d.csail.mit.edu/',
-      citation=_CITATION,
+        # If there's a common (input, target) tuple from the features,
+        # specify them here. They'll be used if as_supervised=True in
+        # builder.as_dataset.
+        supervised_keys=None,
+        # Homepage of the dataset for documentation
+        homepage='http://pix3d.csail.mit.edu/',
+        citation=_CITATION,
     )
 
   def _split_generators(self, dl_manager):
     """Returns SplitGenerators."""
 
     pix3d_dir = dl_manager.download_and_extract(
-      'http://pix3d.csail.mit.edu/data/pix3d.zip')
+        'http://pix3d.csail.mit.edu/data/pix3d.zip')
 
     return [
-      tfds.core.SplitGenerator(
-        name=tfds.Split.TRAIN,
-        gen_kwargs={
-          'samples_directory': pix3d_dir,
-          'split_file': self.TRAIN_SPLIT_IDX
-        },
-      ),
-      tfds.core.SplitGenerator(
-        name=tfds.Split.TEST,
-        gen_kwargs={
-          'samples_directory': pix3d_dir,
-          'split_file': self.TEST_SPLIT_IDX
-        },
-      ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs={
+                'samples_directory': pix3d_dir,
+                'split_file': self.TRAIN_SPLIT_IDX
+            },
+        ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            gen_kwargs={
+                'samples_directory': pix3d_dir,
+                'split_file': self.TEST_SPLIT_IDX
+            },
+        ),
     ]
 
   def _generate_examples(self, samples_directory, split_file):
@@ -182,13 +188,13 @@ class Pix3d(tfds.core.GeneratorBasedBuilder):
       """
       PIX3D_SENSOR_WIDTH = 32.
       return {
-        'pose': {
-          'R': np.array([[-1., 0., 0.], [0., -1., 0.], [0., 0., 1.]],
-                        dtype=np.float32),
-          't': np.zeros(3, dtype=np.float32)
-        },
-        'optical_center': (img_size[0] / 2, img_size[1] / 2),
-        'f': (f / PIX3D_SENSOR_WIDTH * img_size[0])
+          'pose': {
+              'R': np.array([[-1., 0., 0.], [0., -1., 0.], [0., 0., 1.]],
+                            dtype=np.float32),
+              't': np.zeros(3, dtype=np.float32)
+          },
+          'optical_center': (img_size[0] / 2, img_size[1] / 2),
+          'f': (f / PIX3D_SENSOR_WIDTH * img_size[0])
       }
 
     def _build_2d_keypoints(keypoints):
@@ -200,45 +206,46 @@ class Pix3d(tfds.core.GeneratorBasedBuilder):
         raise ValueError("2D keypoints should be in shape (N, M, 2).")
 
       return {
-        'num_annotators': keypoints.shape[0],
-        'num_keypoints': keypoints.shape[1],
-        'keypoints': keypoints.ravel()
+          'num_annotators': keypoints.shape[0],
+          'num_keypoints': keypoints.shape[1],
+          'keypoints': keypoints.ravel()
       }
 
     for sample in split_samples:
       example = {
-        'image': os.path.join(samples_directory, sample['img']),
-        'image/filename': sample['img'],
-        'image/source': sample['img_source'],
-        '2d_keypoints': _build_2d_keypoints(np.asarray(sample['2d_keypoints'],
-                                                       dtype=np.float32)),
-        'mask': os.path.join(samples_directory, sample['mask']),
-        'model': os.path.join(samples_directory, sample['model']),
-        'model/source': sample['model_source'],
-        '3d_keypoints': np.loadtxt(
-          os.path.join(samples_directory, sample['3d_keypoints']),
-          dtype=np.float32),
-        'voxel': {
-          'path': os.path.join(samples_directory, sample['voxel']),
-          'key': 'voxel'
-        },
-        'pose': {
-          'R': sample['rot_mat'],
-          't': sample['trans_mat']
-        },
-        'camera': {
-          'parameters': _build_camera(
-            sample['focal_length'],
-            sample['img_size'],
-          ),
-          'position_with_respect_to_object': sample['cam_position'],
-          'inplane_rotation': sample['inplane_rotation'],
-        },
-        'category': self.CLASS_INDEX.index(sample['category']),
-        'bbox': _build_bbox(sample['bbox'], sample['img_size']),
-        'truncated': sample['truncated'],
-        'occluded': sample['occluded'],
-        'slightly_occluded': sample['slightly_occluded']
+          'image': os.path.join(samples_directory, sample['img']),
+          'image/filename': sample['img'],
+          'image/source': sample['img_source'],
+          '2d_keypoints': _build_2d_keypoints(
+              np.asarray(sample['2d_keypoints'],
+                         dtype=np.float32)),
+          'mask': os.path.join(samples_directory, sample['mask']),
+          'model': os.path.join(samples_directory, sample['model']),
+          'model/source': sample['model_source'],
+          '3d_keypoints': np.loadtxt(
+              os.path.join(samples_directory, sample['3d_keypoints']),
+              dtype=np.float32),
+          'voxel': {
+              'path': os.path.join(samples_directory, sample['voxel']),
+              'key': 'voxel'
+          },
+          'pose': {
+              'R': sample['rot_mat'],
+              't': sample['trans_mat']
+          },
+          'camera': {
+              'parameters': _build_camera(
+                  sample['focal_length'],
+                  sample['img_size'],
+              ),
+              'position_with_respect_to_object': sample['cam_position'],
+              'inplane_rotation': sample['inplane_rotation'],
+          },
+          'category': self.CLASS_INDEX.index(sample['category']),
+          'bbox': _build_bbox(sample['bbox'], sample['img_size']),
+          'truncated': sample['truncated'],
+          'occluded': sample['occluded'],
+          'slightly_occluded': sample['slightly_occluded']
       }
 
       yield sample['img'], example
