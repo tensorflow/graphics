afb14b372c512103afecb2ffabadca32fc73dddb
jamesjiro <sgiron13@verizon.net>
2020-07-22 20:25:37 -0700

    Check inputs in undistortion function.

--- tensorflow_graphics/rendering/camera/tangential_distortion.py
+++ tensorflow_graphics/rendering/camera/tangential_distortion.py
@@ -151,5 +151,107 @@ def distortion_terms(projective_x,
             y_overflow_mask)
 
 
+def undistortion_terms(distorted_projective_x,
+                       distorted_projective_y,
+                       distortion_coefficient_1,
+                       distortion_coefficient_2,
+                       num_iterations=5,
+                       name=None):
+  """Calculates the inverse tangential distortion function given distorted
+  projective coordinates coordinates.
+
+  Given a vector describing a location in camera space in homogeneous
+  coordinates `(x/z, y/z, 1)`, after distortion has been applied, these become
+  `(x'/z, y'/z, 1)`. `distorted_projective_x` is `(x'/z)` and
+  `distorted_projective_y` is `(y'/z)`. `undistortion_terms` are added to `x'/z`
+  and `y'/z` to obtain the undistorted projective coordinates `x/z` and `y/z`.
+  The undistortion_factor in this function is derived from a tangential
+  distortion function, where the distortion factor for x equals
+  `2 * distortion_coefficient_1 * projective_x * projective_y
+  + distortion_coefficient_2 * (squared_radius + 2 * projective_x ** 2)`,
+  and the distortion factor for y equals
+  `2 * distortion_coefficient_2 * projective_x * projective_y
+  + distortion_coefficient_1 * (squared_radius + 2 * projective_y ** 2)`.
+
+  Note:
+    In the following, A1 to An are optional batch dimensions, which must be
+    broadcast compatible.
+
+  Args:
+    distorted_projective_x: A tensor of shape `[A1, ..., An, H, W]` containing
+      the value of projective coordinates `x'/z`.
+    distorted_projective_y: A tensor of shape `[A1, ..., An, H, W]` containing
+      the value of projective coordinates `x'/z`.
+    distortion_coefficient_1: A `scalar` or a tensor of shape `[A1, ..., An]`,
+      which contains the first tangential distortion coefficients of each image.
+    distortion_coefficient_2: A `scalar` or a tensor of shape `[A1, ..., An]`,
+      which contains the second tangential distortion coefficients of each image.
+    num_iterations: Number of Newton-Raphson iterations to calculate the inverse
+      distortion function. Defaults to 5, which is on the high-accuracy side.
+    name: A name for this op. Defaults to
+      "tangential_distortion_undistortion_terms".
+
+  Returns:
+    x_undistortion_term: A tensor of shape `[A1, ..., An, H, W]`, the correction
+      terms that should be added to the distorted projective x coordinates,
+      `x'/z` to obtain the undistorted ones.
+    y_undistortion_term: A tensor of shape `[A1, ..., An, H, W]`, the correction
+      terms that should be added to the distorted projective y coordinates,
+      `y'/z` to obtain the undistorted ones.
+    x_overflow_mask: A boolean tensor of shape `[A1, ..., An, H, W]`, `True`
+      where `distorted_projective_x` is beyond the range where the distortion function is
+      monotonically increasing. Wherever `overflow_mask` is True,
+      `x_undistortion_term`'s value is meaningless.
+    y_overflow_mask: A boolean tensor of shape `[A1, ..., An, H, W]`, `True`
+      where `distorted_projective_y` is beyond the range where the distortion function is
+      monotonically increasing. Wherever `overflow_mask` is True,
+      `y_undistortion_terms`'s value is meaningless.
+  """
+  with tf.compat.v1.name_scope(name,
+                               "tangential_distortion_undistortion_terms",
+                               [distorted_projective_x,
+                                distorted_projective_y,
+                                distortion_coefficient_1,
+                                distortion_coefficient_2]):
+    distorted_projective_x = tf.convert_to_tensor(value=distorted_projective_x)
+    distorted_projective_y = tf.convert_to_tensor(value=distorted_projective_y)
+    distortion_coefficient_1 = tf.convert_to_tensor(
+      value=distortion_coefficient_1)
+    distortion_coefficient_2 = tf.convert_to_tensor(
+      value=distortion_coefficient_2)
+
+    if distortion_coefficient_1.shape.ndims == 0:
+      distortion_coefficient_1 = tf.expand_dims(distortion_coefficient_1, axis=0)
+    if distortion_coefficient_2.shape.ndims == 0:
+      distortion_coefficient_2 = tf.expand_dims(distortion_coefficient_2, axis=0)
+    shape.check_static(
+      tensor=distorted_projective_x,
+      tensor_name="distorted_projective_x",
+      has_rank_greater_than=1)
+    shape.check_static(
+      tensor=distorted_projective_y,
+      tensor_name="distorted_projective_y",
+      has_rank_greater_than=1)
+    shape.compare_batch_dimensions(
+      tensors=(distorted_projective_x,
+               distorted_projective_y,
+               distortion_coefficient_1,
+               distortion_coefficient_2),
+      last_axes=(-3, -3, -1, -1),
+      broadcast_compatible=True)
+    distorted_projective_x = asserts.assert_all_above(
+      distorted_projective_x, 0.0, open_bound=False)
+    distorted_projective_y = asserts.assert_all_above(
+      distorted_projective_y, 0.0, open_bound=False)
+    distortion_coefficient_1 = tf.expand_dims(distortion_coefficient_1, axis=-1)
+    distortion_coefficient_1 = tf.expand_dims(distortion_coefficient_1, axis=-1)
+    distortion_coefficient_2 = tf.expand_dims(distortion_coefficient_2, axis=-1)
+    distortion_coefficient_2 = tf.expand_dims(distortion_coefficient_2, axis=-1)
+    distorted_squared_radius = (
+      distorted_projective_x ** 2.0 + distorted_projective_y ** 2.0)
+
+    return None
+
+
 # API contains all public functions and classes.
 __all__ = export_api.get_functions_and_classes()
