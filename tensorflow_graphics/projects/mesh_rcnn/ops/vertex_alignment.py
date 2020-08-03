99388a1ca3fdfa4499f346c1f31a0bd0ec8c6a3f
Robin Baumann <mail@robin-baumann.com>
2020-08-03 14:57:27 +0200

    Build VertAlign using TFG perspective cam and TFA bilinear interpolate

--- tensorflow_graphics/projects/mesh_rcnn/ops/vertex_alignment.py
+++ tensorflow_graphics/projects/mesh_rcnn/ops/vertex_alignment.py
@@ -12,12 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Implementation of the vert align operation for Mesh R-CNN.
+Implementation of the VertAlign operation for Mesh R-CNN.
 
 This operation is also called 'perceptual feature pooling' in Wang et al.
 
-Mesh R-CNN uses bilinear interpolation and border padding. Thus, this
-implementation does not provide other algorithms.
+Given the 3D coordinate of a
+vertex, this OP calculates its 2D projection on input image plane using camera
+intrinsics, and then pool the feature from four nearby pixels using
+bilinear interpolation.
 
 References:
   * Georgia Gkioxari, Jitendra Malik, & Justin Johnson. (2019). Mesh R-CNN.
@@ -26,10 +28,13 @@ References:
 """
 
 import tensorflow as tf
+import tensorflow_addons as tfa
+from tensorflow_graphics.rendering.camera import perspective
 
 
 def vert_align(features,
-               vertices):
+               vertices,
+               intrinsics):
   """
   Sample vertex features from a feature map.
 
@@ -38,25 +43,34 @@ def vert_align(features,
       from which to sample the features. N is a batch dimension.
     vertices: list of N float32 tensors of shape `[V, 3]` containing the vertex
       positions for which to sample the image features.
+    intrinsics: float32 tensor of shape `[N, 3, 3]` representing the intrinsic
+      camera matrix.
 
   Returns:
     float32 tensor of shape `[N, V, C]` containing sampled features per vertex.
   """
 
   features = tf.convert_to_tensor(features)
+  intrinsics = tf.convert_to_tensor(intrinsics)
 
   if not all([v.shape.rank == 2 for v in vertices]):
     raise ValueError('vertices should be 2 dimensional.')
 
   if not features._rank() == 4:
     raise ValueError('features must of shape (N, H, W, C).')
-  verts_2d = [tf.cast(v[..., :2], tf.float32) for v in vertices]
-  grid = _spatial_normalize_grid_to_feature(verts_2d, features)
-  padded_grid, padding_offsets = pad_query_points(grid)
-  print(padded_grid.shape)
-  sample_grid = tf.reshape(padded_grid, (features.shape[0], 1, -1, 2))
-  print(sample_grid.shape)
-  sampled_features = grid_sample_2d(features, sample_grid)
+
+
+  padded_grid, padding_offsets = pad_query_points(vertices)
+
+  focal_length = tf.linalg.diag_part(intrinsics)[..., :2]
+  principal_point = tf.squeeze(tf.gather(intrinsics, [2], axis=1)[:2])
+  projected_vertices = perspective.project(padded_grid,
+                                           focal_length,
+                                           principal_point)
+  print(projected_vertices)
+  sampled_features = tfa.image.interpolate_bilinear(features,
+                                                    projected_vertices,
+                                                    indexing='ij')
 
   return [s_feat[:padding_offsets[i]] for i, s_feat in
           enumerate(sampled_features)]
@@ -78,8 +92,8 @@ def pad_query_points(points):
   padded_vertices = []
   original_lengths = []
   for point in points:
-    if point.shape[-1] < 2:
-      raise ValueError('points.shape[-1] has to be at least 2.')
+    if point.shape[-1] != 3:
+      raise ValueError('points.shape[-1] has to be 3.')
 
     original_lengths.append(point.shape[0])
     pad_length = max_num_points - point.shape[0]
@@ -90,80 +104,3 @@ def pad_query_points(points):
       padded_vertices.append(tf.concat([point, pad], 0))
 
   return tf.stack(padded_vertices), original_lengths
-
-
-def _spatial_normalize_grid_to_feature(grids, feature_map):
-  """
-  Normalize batch of 2D coordinates (x, y) such that (-1, -1) corresponds to
-  top-left and (+1, +1) to bottom-right location in the input feature map.
-
-  Args:
-    grids: list of `N1` float32 tensors of shape `[N2,2]` containing the 2D
-    coordinates that should be normalized.
-    feature_map: float32 tensor of shape `[N1, H, W, C]` that is used to normalize
-      spatial dimension leaving the channels C untouched.
-
-  Returns:
-    list with `N1` float32 tensors of shape `[N2,2]` with normalized coordinates.
-  """
-  if not all([g.shape[-1] == 2 for g in grids]):
-    raise ValueError('Grid must contain 2D coordinats.')
-  if feature_map._rank() != 4:
-    raise ValueError('feature_map must be a tensor of rank 4.')
-  normalized_grids = []
-  for i, grid in enumerate(grids):
-    H, W, _ = feature_map[i].shape
-    extent = tf.constant([H / 2, W / 2], dtype=tf.float32)
-    normalized_grids.append((grid - extent) / extent)
-  return normalized_grids
-
-
-def grid_sample_2d(source, grid):
-  """
-  Performs 2D grid sampling with bilinear interpolation of the source according
-  to normalized coordinates provided by the grid.
-
-  Args:
-    source: float32 tensor of shape `[N, H, W, C]` representing the source
-      from which to sample.
-    grid: float32 tensor of shape `[N, X, Y]` containing the samling locations.
-
-  Returns:
-    float32 tensor of shape `[N, X, Y, C]` containing the result of the
-      interpolation.
-  """
-
-  N, H, W, _ = source.shape
-
-  # Find interpolation neighbours
-  y, x = grid[..., 0], grid[..., 1]
-  y = tf.cast(H - 1, grid.dtype) * (y + 1) / 2
-  x = tf.cast(W - 1, grid.dtype) * (x + 1) / 2
-  low = tf.maximum(tf.cast(tf.floor(y), tf.int32), 0)
-  high = tf.minimum(low + 1, H - 1)
-  right = tf.maximum(tf.cast(tf.floor(x), tf.int32), 0)
-  left = tf.minimum(right + 1, W - 1)
-
-  # Gather pixel values
-  index = tf.tile(tf.range(N)[:, None, None],
-                  tf.concat([[1], tf.shape(y)[1:]], axis=0))
-  value_bottom_right = tf.gather_nd(source,
-                                    tf.stack([index, low, right], axis=-1))
-  value_bottom_left = tf.gather_nd(source,
-                                   tf.stack([index, low, left], axis=-1))
-  value_top_right = tf.gather_nd(source,
-                                 tf.stack([index, high, right], axis=-1))
-  value_top_left = tf.gather_nd(source, tf.stack([index, high, left], axis=-1))
-
-  # Interpolation coefficients
-  d_y = tf.cast(y, source.dtype) - tf.cast(low, source.dtype)
-  d_y = tf.expand_dims(d_y, -1)
-  d_x = tf.cast(x, source.dtype) - tf.cast(right, source.dtype)
-  d_x = tf.expand_dims(d_x, -1)
-
-  # Compute bilinear interpolation
-  inter_y_right = value_bottom_right * (1 - d_y) + value_top_right * d_y
-  inter_y_left = value_bottom_left * (1 - d_y) + value_top_left * d_y
-  interpolated = inter_y_right * (1 - d_x) + inter_y_left * d_x
-
-  return interpolated
--- tensorflow_graphics/projects/mesh_rcnn/ops/vertex_alignment_test.py
+++ tensorflow_graphics/projects/mesh_rcnn/ops/vertex_alignment_test.py
@@ -25,18 +25,18 @@ from tensorflow_graphics.util import test_case
 class VertAlignTest(test_case.TestCase):
   """Test Cases for vert align OP."""
 
-  def test_vert_align_without_interpolation(self):
+  def test_vert_align_with_matching_querypoints(self):
     """Test on set of points, where H & W dimensions are equal to source."""
-    N, H, W = 2, 10, 10
-    source = tf.random.normal((N, H, W, 3), dtype=tf.float32)
-    grid = tf.meshgrid(tf.range(0, limit=W, dtype=tf.float32),
-                       tf.range(0, limit=H, dtype=tf.float32))
-    batch_query_points = tf.reshape(tf.stack(grid, axis=-1), (-1, 2))
-    batch_query_points = tf.concat(
-        [batch_query_points, tf.ones((H * W, 1))], axis=-1)
-    query_points = [batch_query_points] * N
-    output = vert_align(source, query_points)
-    output_tensor = tf.convert_to_tensor(output)
-    expected_output = tf.reshape(source, (N, 1, -1, 3))
 
-    self.assertAllClose(expected_output, output_tensor)
+    image = tf.reshape(tf.range(20.), (1, 4, 5, 1))
+    verts = tf.constant([[-1.5, -1.5, 10],
+                         [-1.5, 0.5, 10],
+                         [-0.5, -1.5, 10],
+                         [-0.5, 0.5, 10]], dtype=tf.float32)
+    intrinsics = tf.constant([[10, 0, 2.5], [0, 10, 2.5], [0, 0, 1]],
+                             dtype=tf.float32)
+
+    expected_result = tf.constant([[6.], [8.], [11.], [13.]])
+    result = vert_align(image, [verts], intrinsics)
+
+    self.assertAllClose(expected_result, result[0])
