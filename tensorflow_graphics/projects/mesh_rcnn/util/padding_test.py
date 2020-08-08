d707c8c3bdfcce0a88e91ff8fccb564f2ba44333
Robin Baumann <mail@robin-baumann.com>
2020-08-08 13:08:09 +0200

    Make vert align accept and return padded tensors instead of lists

--- tensorflow_graphics/projects/mesh_rcnn/util/padding_test.py
+++ tensorflow_graphics/projects/mesh_rcnn/util/padding_test.py
@@ -25,15 +25,14 @@ class PaddingTest(test_case.TestCase):
 
   def test_simple_padding(self):
     """Test with 2 tensors of different shape"""
-    values = [tf.ones([1, 4, 3]), tf.ones([1, 2, 3])]
+    values = [tf.ones([4, 3]), tf.ones([2, 3])]
     result, sizes = pad_list(values)
-    print(result, sizes)
     self.assertTrue([2, 4, 3] == result.shape)
     self.assertTrue(sizes[1] == 2)
 
   def test_raises(self):
     """2 Tensors, different rank. Should raise ValueError."""
-    values = [tf.ones([1, 4, 3]), tf.ones([1, 2, 2, 3])]
+    values = [tf.ones([4, 3]), tf.ones([2, 2, 3])]
     with self.assertRaisesWithPredicateMatch(
         ValueError, 'All tensors need to have same rank.'):
       _, _ = pad_list(values)
@@ -42,15 +41,21 @@ class PaddingTest(test_case.TestCase):
     """Tests with 2 tensors of same rank, but padding in more than one
     dimension is requiered. This should raise ValueError."""
 
-    values = [tf.ones([1, 4, 4, 3]), tf.ones([1, 2, 2, 3])]
+    values = [tf.ones([4, 4, 3]), tf.ones([2, 2, 3])]
     with self.assertRaisesWithPredicateMatch(
         ValueError, 'Only one dimension with unequal length supported.'):
       _, _ = pad_list(values)
 
   def test_batch_padd(self):
     """Tests padding for multiple batch dimensions."""
-    values = [tf.ones([1, 4, 2, 3]), tf.ones([1, 2, 2, 3])]
+    values = [tf.ones([4, 2, 3]), tf.ones([2, 2, 3])]
     result, sizes = pad_list(values)
-    print(result)
     self.assertTrue([2, 4, 2, 3] == result.shape)
     self.assertTrue(sizes[1] == 2)
+
+  def test_batch_with_equally_long_elements(self):
+    """Test with 2 tensors of same shape"""
+    values = [tf.ones([4, 3]), tf.ones([4, 3])]
+    result, sizes = pad_list(values)
+    self.assertTrue([2, 4, 3] == result.shape)
+    self.assertTrue(sizes[1] == 0)
