Fix test cases for empty meshes and add batch broadcasting to VertAlign

tensorflow_graphics/projects/mesh_rcnn/ops/cubify_test.py
@@ -31,8 +31,8 @@ class CufifyTest(test_case.TestCase):
     voxels = tf.random.uniform((N, V, V, V), 0, 0.5, tf.float32)
     mesh = cubify(voxels, threshold=0.7)
     vertices, faces = mesh.get_unpadded()
-    self.assertEmpty(vertices[0])
-    self.assertEmpty(faces[0])
+    self.assertEmpty(vertices[0][0])
+    self.assertEmpty(faces[0][0])
 
   def test_cubify_on_cube(self):
     """Test cubify on arbitrary cubes."""

tensorflow_graphics/projects/mesh_rcnn/ops/vertex_alignment.py
@@ -29,7 +29,9 @@ References:
 
 import tensorflow as tf
 import tensorflow_addons as tfa
+
 from tensorflow_graphics.rendering.camera import perspective
+from tensorflow_graphics.util import shape
 
 
 def vert_align(features,
@@ -59,8 +61,16 @@ def vert_align(features,
   if tf.rank(features) != 4:
     raise ValueError('features must of shape (N, H, W, C).')
 
-  focal_length = tf.linalg.diag_part(intrinsics)[..., :2]
-  principal_point = tf.squeeze(tf.gather(intrinsics, [2], axis=1)[:2])
+  if tf.rank(intrinsics) != 3:
+    raise ValueError('intrinsics must of shape (N, 3, 3).')
+
+  shape.compare_batch_dimensions([vertices, features, intrinsics],
+                                 last_axes=0,
+                                 broadcast_compatible=True)
+
+  focal_length = tf.expand_dims(tf.linalg.diag_part(intrinsics)[..., :2], -2)
+  principal_point = tf.expand_dims(
+      tf.squeeze(tf.gather(intrinsics, [2], axis=-1)[:, :2]), -2)
   projected_vertices = perspective.project(vertices,
                                            focal_length,
                                            principal_point)

tensorflow_graphics/projects/mesh_rcnn/ops/vertex_alignment_test.py
@@ -39,9 +39,9 @@ class VertAlignTest(test_case.TestCase):
     batched_verts = tf.expand_dims(verts, 0)
     intrinsics = tf.constant([[10, 0, 2.5], [0, 10, 2.5], [0, 0, 1]],
                              dtype=tf.float32)
-
+    batched_intrinsics = tf.expand_dims(intrinsics, 0)
     expected_result = tf.constant([[6.], [8.], [11.], [13.]])
-    result = vert_align(image, batched_verts, intrinsics)
+    result = vert_align(image, batched_verts, batched_intrinsics)
 
     self.assertAllClose(expected_result, result[0])
 
@@ -59,8 +59,9 @@ class VertAlignTest(test_case.TestCase):
     batched_verts = tf.expand_dims(verts, 0)
     intrinsics = tf.constant([[10, 0, 5], [0, 10, 3.5], [0, 0, 1]],
                              dtype=tf.float32)
+    batched_intrinsics = tf.expand_dims(intrinsics, 0)
     expected_result = tf.constant([[8.], [12.], [57.], [61.]])
-    result = vert_align(image, batched_verts, intrinsics)
+    result = vert_align(image, batched_verts, batched_intrinsics)
 
     self.assertAllClose(expected_result, result[0])
 
@@ -75,13 +76,13 @@ class VertAlignTest(test_case.TestCase):
     batched_verts = tf.expand_dims(verts, 0)
     intrinsics = tf.constant([[10, 0, 2.5], [0, 10, 2.5], [0, 0, 1]],
                              dtype=tf.float32)
-
+    batched_intrinsics = tf.expand_dims(intrinsics, 0)
     expected_result = tf.constant([image[0, 1, 1, :].numpy(),
                                    image[0, 1, 3, :].numpy(),
                                    image[0, 2, 1, :].numpy(),
                                    image[0, 2, 3, :].numpy()])
 
-    result = vert_align(image, batched_verts, intrinsics)
+    result = vert_align(image, batched_verts, batched_intrinsics)
 
     self.assertAllClose(expected_result, result[0])
 
@@ -99,6 +100,7 @@ class VertAlignTest(test_case.TestCase):
 
     intrinsics = tf.constant([[10, 0, 2.5], [0, 10, 2.5], [0, 0, 1]],
                              dtype=tf.float32)
+    intrinsics = tf.stack([intrinsics, intrinsics])
 
     input_mesh = mesh.Meshes([verts1, verts2],
                              [tf.ones((2, 3)), tf.ones((2, 3))])

tensorflow_graphics/projects/mesh_rcnn/structures/mesh_test.py
@@ -111,9 +111,9 @@ class MeshTest(test_case.TestCase):
     empty_mesh = mesh.Meshes([tf.constant([], dtype=tf.float32)], [
         tf.constant([], dtype=tf.float32)])
 
-    self.assertEmpty(empty_mesh.get_unpadded()[0][0])
-    self.assertEmpty(empty_mesh.get_unpadded()[1][0])
-    self.assertEmpty(empty_mesh.get_padded()[0])
-    self.assertEmpty(empty_mesh.get_padded()[1])
-    self.assertEmpty(empty_mesh.get_flattened()[0])
-    self.assertEmpty(empty_mesh.get_flattened()[1])
+    self.assertEmpty(empty_mesh.get_unpadded()[0][0][0])
+    self.assertEmpty(empty_mesh.get_unpadded()[1][0][0])
+    self.assertEmpty(empty_mesh.get_padded()[0][0])
+    self.assertEmpty(empty_mesh.get_padded()[1][0])
+    self.assertEmpty(empty_mesh.get_flattened()[0][0])
+    self.assertEmpty(empty_mesh.get_flattened()[1][0])

tensorflow_graphics/projects/mesh_rcnn/util/padding_test.py
@@ -67,7 +67,7 @@ class PaddingTest(test_case.TestCase):
     """Test call with single tensor for additional batch dimension"""
     values = [tf.ones([4, 3])]
     expected_result = tf.ones((1, 4, 3))
-    result, sizes = pad_list(values)
+    result, _ = pad_list(values)
 
     self.assertEqual(expected_result.shape, result.shape)
     self.assertAllEqual(expected_result, result)