9f8434b84926b4cf241cab6a7b66e375a4233d6a
Robin Baumann <mail@robin-baumann.com>
2020-08-24 14:18:50 +0200

    Checkout structures, util modules from  GSoC20_Mesh_R-CNN_MeshRefinement

---
+++ tensorflow_graphics/projects/mesh_rcnn/structures/mesh.py
@@ -0,0 +1,269 @@
+# Copyright 2020 The TensorFlow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Data Structure for Meshes supporting different representations."""
+
+import tensorflow as tf
+
+import tensorflow_graphics.geometry.convolution.utils as utils
+from tensorflow_graphics.projects.mesh_rcnn.util import padding
+
+
+class Meshes:
+  """Wrapper for batching of Meshes with unequal size."""
+
+  def __init__(self, vertices, faces, batch_sizes=None):
+    """
+    Contstructs the wrapper object from nested lists of multiple vertices and
+    faces.
+
+    Internally, vertices and faces are padded and packed into a single 2D
+    tensor. The object provides accessors for different representations,
+    including:
+
+    * List: List of all unpadded vertices and faces tensors. This is intended as
+        input format only. However, it is possible to retrieve a flat list of
+        all vertices and faces tensor via `get_unpadded()`.
+    * Padded: Tensor of shape `[A1, ..., An, max(N), 3]`, where max(N) is the
+        size of the largest vertex or face list in all batch dimensions. All
+        other vertex/face tensors get padded to this size.
+    * Flattened: 2D tensor of shape `[sum(N1, ..., Nn), 3]`, where N1, ..., Nn
+        are the sizes of every vertex or face list in this batch.
+
+    Note:
+      This implementation supports arbitrary batch dimensions by passing in the
+      batch shapes in `batch_sizes`. The vertex and face lists must be flattened
+      and reshapeable to the shape defined in `batch_sizes`. E.g. consider the
+      case where 4 different meshes are to be stored in a Meshes object, the
+      batched shape could be `[2, 2]`, `[4, 1]` or [`1, 4`]. In other words, the
+      product over all entries in `batch_sizes` must be equal to the number of
+      vertices and faces provided in the constructor.
+
+    Args:
+      vertices: List with N float32 tensors of shape `[V, 3]` containing the
+        mesh vertices, or empty list.
+      faces: List with N float32 tensors of shape `[F, 3]` containing the mesh
+        faces, or empty list.
+      batch_sizes: Optinal list of ints indicating the size of each batch
+        dimension for the meshes or None, if there is only one batch
+        dimension.
+    """
+    if batch_sizes is not None:
+      batch_sizes = tf.convert_to_tensor(batch_sizes)
+      # Set batch_sizes to None if only single value was provided, since this is
+      # the default behavior.
+      batch_sizes = None if tf.size(batch_sizes) == 1 else batch_sizes
+
+    self._check_valid_input(vertices, faces, batch_sizes)
+
+    self.is_empty = len(vertices) == 0
+
+    if not self.is_empty:
+      vertices, vertex_sizes = padding.pad_list(vertices)
+      faces, face_sizes = padding.pad_list(faces)
+
+      if batch_sizes is not None:
+        vert_shape = tf.concat([batch_sizes, vertices.shape[1:]], 0)
+        face_shape = tf.concat([batch_sizes, faces.shape[1:]], 0)
+
+        vertices = tf.reshape(vertices, vert_shape)
+        faces = tf.reshape(faces, face_shape)
+
+        vertex_sizes = tf.reshape(vertex_sizes, batch_sizes)
+        face_sizes = tf.reshape(face_sizes, batch_sizes)
+
+      self.vertex_sizes = vertex_sizes
+      self.face_sizes = face_sizes
+
+      self.vertices, self._unfold_vertices = utils.flatten_batch_to_2d(
+          vertices, sizes=self.vertex_sizes)
+      self.faces, self._unfold_faces = utils.flatten_batch_to_2d(
+          faces,
+          sizes=self.face_sizes)
+
+      # adjacency will be computed on first call of self.vertex_neighbors()
+      self.vertex_adjacency = None
+
+  def get_flattened(self):
+    """
+    Returns the flattened vertices and faces.
+    Returns:
+      A 2D tensor of shape `[N*V, 3]` containing all padded vertices.
+      A 2D tensor of shape  `[N*F, 3]` containing all padded faces.
+    """
+    if self.is_empty:
+      vertices = tf.zeros([0, 3], dtype=tf.float32)
+      faces = tf.zeros([0, 3], dtype=tf.int32)
+    else:
+      vertices, faces = self.vertices, self.faces
+
+    return vertices, faces
+
+  def get_padded(self):
+    """
+    Unpacks vertices and faces and returns them as a padded tensor.
+    Returns:
+      A tensor of shape `[N, V, 3]` containing the padded vertices and
+      a tensor of shape `[N, F, 3]` containing the padded faces.
+
+    """
+    if self.is_empty:
+      vertices = tf.zeros([0, 3], dtype=tf.float32)
+      faces = tf.zeros([0, 3], dtype=tf.int32)
+    else:
+      vertices = self._unfold_vertices(self.vertices)
+      faces = self._unfold_faces(self.faces)
+
+    return vertices, faces
+
+  def get_unpadded(self):
+    """
+    Unpads und unstacks all vertices and faces and returns them as a flat list.
+
+    Returns:
+      A list of N vertex tensors of shape `[V',3]`.
+      A list of N face tensors of shape `[F',3]`
+    """
+    vertices, faces = self.get_padded()
+
+    if tf.rank(vertices) < 3:
+      return [vertices], [faces]
+
+    vertex_sizes = tf.reshape(self.vertex_sizes, (-1))
+    face_sizes = tf.reshape(self.face_sizes, (-1))
+
+    vertices = tf.reshape(vertices, [-1] + vertices.shape[-2:].as_list())
+    faces = tf.reshape(faces, [-1] + faces.shape[-2:].as_list())
+    vertices = [vertex[:vertex_sizes[i]] for i, vertex in
+                enumerate(vertices)]
+    faces = [face[:face_sizes[i]] for i, face in enumerate(faces)]
+
+    return vertices, faces
+
+  def get_sizes(self):
+    """Return the sizes tensors."""
+    return self.vertex_sizes, self.face_sizes
+
+  def add_offsets(self, offsets):
+    """
+    Adds offsets to mesh vertices, changing the meshes geomtry but
+    preserving the topology.
+
+    Args:
+      offsets:  float32 tensor of same shape as self.vertices.
+    """
+    if offsets.shape != self.vertices.shape:
+      raise ValueError(
+          f'Offsets must be a tensor of shape as {self.vertices.shape}!')
+
+    self.vertices = self.vertices + offsets
+
+    return self
+
+  def _check_valid_input(self, vertices, faces, batch_sizes):
+    """
+    Checks if the provided input is valid.
+
+    Args:
+      vertices: List of vertices provided in constructor
+      faces: List of faces provided in constructor
+      batch_sizes: List of tuple of ints provided in constructor or None
+
+    Raises:
+      ValueError: if the input is not of a valid formtat.
+    """
+    if len(vertices) != len(faces):
+      raise ValueError('Need as many face-lists as vertex-lists.')
+
+    if batch_sizes is not None:
+      if not len(vertices) % tf.reduce_prod(batch_sizes) == 0:
+        raise ValueError(f'vertices list of size {len(vertices)} cannot be '
+                         f'batched to shape {batch_sizes}!')
+
+      if not len(faces) % tf.reduce_prod(batch_sizes) == 0:
+        raise ValueError(f'vertices list of size {len(faces)} cannot be '
+                         f'batched to shape {batch_sizes}!')
+
+
+  def vertex_neighbors(self):
+    """
+    For each vertex i, find all vertices in the 1-ring of vertex i.
+
+    The adjacency matrix will be only computed once, as the meshes are not
+    expected to change topology.
+
+    Note:
+      In the following, A1 to An are optional batch dimensions that must be
+      broadcast compatible.
+
+    Returns:
+      float32 SparseTensor of shape `[sum(V1, ..., Vn), sum(V1, ..., Vn)]` where
+      V1, ... Vn represent the number of vertices of each mesh in the batch.
+    """
+    if self.vertex_adjacency is None:
+      self.vertex_adjacency = self._compute_vertex_adjacency()
+
+    return self.vertex_adjacency
+
+  def _compute_vertex_adjacency(self):
+    """For each vertex i, find all vertices in the 1-ring of vertex i.
+    This method at first computes all unique bidirectional edges per batch
+    instance and builds a SparseTensor representing the adjacency matrix.
+    The returned adjacency matrix is represented as a SparseTensor of shape
+    `[sum(V1, ..., Vn), sum(V1, ..., Vn)]` where V1, ... Vn represent the
+    number of vertices of each mesh in the batch.
+    """
+    vertices, faces = self.get_padded()
+    edges = tf.concat(
+        [faces[..., 0:2], faces[..., 1:3], tf.gather(faces, [2, 0], axis=-1)],
+        -2)
+    n_verts = vertices.shape[-2]
+
+    # hash edges, so that we can use tf.unique, which currently only supports
+    # 1D tensors as input.
+    edges_hashed = n_verts * edges[..., 0] + edges[..., 1]
+
+    indices = []
+    for b_id, batch in enumerate(edges_hashed):
+      unique, _ = tf.unique(batch)
+      # retrieve vertices from hash value
+      edges_padded = tf.stack([unique // n_verts, unique % n_verts], axis=1)
+      # padding is now not necessarily at the end of the tensor, hence keep only
+      # edges that connect two different vertices
+      padding_mask = tf.not_equal(edges_padded[:, 0], edges_padded[:, 1])
+      edges = tf.boolean_mask(edges_padded, padding_mask)
+      # since we consider bidirectional edges, concat reversed index
+      edges_reversed = tf.reverse(edges, axis=[-1])
+      symmetric = tf.concat([edges, edges_reversed], 0)
+
+      # Also include vertex v into its 1-ring
+      batch_verts = tf.range(self.vertex_sizes[b_id], dtype=tf.int32)
+      self_refs = tf.stack([batch_verts, batch_verts], 1)
+      index = tf.concat([symmetric, self_refs], 0)
+
+      # add batch offset, so that we can later construct the SparseTensor for the
+      # whole batch
+      batch_offset = tf.reduce_sum(self.vertex_sizes[:b_id])
+      batch_index = index + batch_offset
+
+      indices.append(batch_index)
+
+    sparse_indices = tf.cast(tf.concat(indices, 0), tf.int64)
+    side_length = tf.reduce_sum(self.vertex_sizes)
+    neighborhoods = tf.SparseTensor(
+        sparse_indices,
+        tf.ones(len(sparse_indices), dtype=self.vertices.dtype),
+        dense_shape=(side_length, side_length)
+    )
+
+    return tf.sparse.reorder(neighborhoods)
---
+++ tensorflow_graphics/projects/mesh_rcnn/structures/mesh_test.py
@@ -0,0 +1,196 @@
+# Copyright 2020 The TensorFlow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Test cases for Mesh wrapper."""
+
+from absl.testing import parameterized
+import tensorflow as tf
+
+from tensorflow_graphics.projects.mesh_rcnn.structures.mesh import Meshes
+from tensorflow_graphics.util import test_case
+
+
+class MeshTest(test_case.TestCase):
+  """Test cases for Mesh wrapper."""
+
+  def test_mesh_packing_on_single_mesh(self):
+    """Tests packing and unpacking of meshes with a single mesh."""
+    unit_cube_verts = tf.constant(
+        [
+            [0, 0, 0],
+            [0, 0, 1],
+            [0, 1, 0],
+            [0, 1, 1],
+            [1, 0, 0],
+            [1, 0, 1],
+            [1, 1, 0],
+            [1, 1, 1],
+        ], dtype=tf.int32)
+
+    unit_cube_faces = tf.constant(
+        [
+            [0, 1, 2],
+            [1, 3, 2],  # left face: 0, 1
+            [2, 3, 6],
+            [3, 7, 6],  # bottom face: 2, 3
+            [0, 2, 6],
+            [0, 6, 4],  # front face: 4, 5
+            [0, 5, 1],
+            [0, 4, 5],  # up face: 6, 7
+            [6, 7, 5],
+            [6, 5, 4],  # right face: 8, 9
+            [1, 7, 3],
+            [1, 5, 7],  # back face: 10, 11
+        ],
+        dtype=tf.int32
+    )
+
+    unit_cube_mesh = Meshes([unit_cube_verts], [unit_cube_faces])
+
+    expected_flat_vertices = tf.reshape(unit_cube_verts, (-1, 3))
+    expected_flat_faces = tf.reshape(unit_cube_faces, (-1, 3))
+
+    expected_padded_verts = tf.expand_dims(unit_cube_verts, 0)
+    expected_padded_faces = tf.expand_dims(unit_cube_faces, 0)
+
+    self.assertAllEqual(expected_flat_vertices,
+                        unit_cube_mesh.get_flattened()[0])
+    self.assertAllEqual(expected_flat_faces, unit_cube_mesh.get_flattened()[1])
+
+    self.assertAllEqual(expected_padded_verts, unit_cube_mesh.get_padded()[0])
+    self.assertAllEqual(expected_padded_faces, unit_cube_mesh.get_padded()[1])
+
+    self.assertAllEqual(unit_cube_verts, unit_cube_mesh.get_unpadded()[0][0])
+    self.assertAllEqual(unit_cube_faces, unit_cube_mesh.get_unpadded()[1][0])
+
+  def test_mesh_packing_on_batch(self):
+    """Tests packing and unpacking of meshes in a batch."""
+
+    short_verts = tf.reshape(tf.range(12.), (4, 3))
+    long_verts = tf.reshape(tf.range(12., 36.), (8, 3))
+
+    short_faces = tf.reshape(tf.range(12), (4, 3))
+    long_faces = tf.reshape(tf.range(12, 36), (8, 3))
+
+    vertices = [long_verts, short_verts]
+    faces = [long_faces, short_faces]
+
+    meshes = Meshes(vertices, faces)
+
+    padded_short_verts = tf.pad(short_verts, [[0, 4], [0, 0]], 'CONSTANT', 0)
+    padded_short_faces = tf.pad(short_faces, [[0, 4], [0, 0]], 'CONSTANT', 0)
+
+    batched_verts = tf.stack([long_verts, padded_short_verts], axis=0)
+    batched_faces = tf.stack([long_faces, padded_short_faces], axis=0)
+
+    expected_flat_verts = tf.reshape(batched_verts, (16, 3))[:-4]
+    expected_flat_faces = tf.reshape(batched_faces, (16, 3))[:-4]
+
+    # Check flat representation
+    self.assertAllEqual(expected_flat_verts, meshes.get_flattened()[0])
+    self.assertAllEqual(expected_flat_faces, meshes.get_flattened()[1])
+
+    # check padded representation
+    self.assertAllEqual(batched_verts, meshes.get_padded()[0])
+    self.assertAllEqual(batched_faces, meshes.get_padded()[1])
+
+    # check unpadding
+    self.assertAllEqual(vertices[0], meshes.get_unpadded()[0][0])
+    self.assertAllEqual(faces[0], meshes.get_unpadded()[1][0])
+    self.assertAllEqual(vertices[1], meshes.get_unpadded()[0][1])
+    self.assertAllEqual(faces[1], meshes.get_unpadded()[1][1])
+
+  def test_with_empty_lists(self):
+    """Tests implementation with empty meshes."""
+
+    with self.assertRaises(ValueError):
+      _ = Meshes([tf.constant([], dtype=tf.float32)],
+                 [tf.constant([], dtype=tf.float32)])
+
+  def test_adjacency(self):
+    """Tests computation of vertex adjacencies."""
+    verts = [
+        tf.constant([[0, 0, 0],
+                     [1, 1, 1],
+                     [2, 2, 2],
+                     [3, 3, 3]]),
+        tf.constant([[0, 0, 0],
+                     [1, 1, 1],
+                     [2, 2, 2],
+                     [3, 3, 3],
+                     [4, 4, 4]])
+    ]
+
+    faces = [
+        tf.constant([[0, 1, 2], [0, 1, 3]]),
+        tf.constant([[0, 1, 2], [0, 1, 3], [1, 2, 4]])
+    ]
+
+    meshes = Meshes(verts, faces)
+    adjacency = meshes.vertex_neighbors()
+    expected_adjacency = tf.constant(
+        [[1, 1, 1, 1, 0, 0, 0, 0, 0],
+         [1, 1, 1, 1, 0, 0, 0, 0, 0],
+         [1, 1, 1, 0, 0, 0, 0, 0, 0],
+         [1, 1, 0, 1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 1, 1, 1, 0],
+         [0, 0, 0, 0, 1, 1, 1, 1, 1],
+         [0, 0, 0, 0, 1, 1, 1, 0, 1],
+         [0, 0, 0, 0, 1, 1, 0, 1, 0],
+         [0, 0, 0, 0, 0, 1, 1, 0, 1]])
+    self.assertAllEqual(expected_adjacency, tf.sparse.to_dense(adjacency))
+
+  @parameterized.parameters(
+      (4, 1),
+      (2, 2),
+      (1, 4)
+  )
+  def test_multiple_batch_dimensions(self, b1, b2):
+    """Tests implementation with multiple batch dimensions containing meshes of
+    different size and one empty mesh."""
+
+    verts1 = tf.constant([[0, 0, 0], [1, 0, 0], [1, 1, 0]], dtype=tf.float32)
+    faces1 = tf.constant([[0, 1, 2]], dtype=tf.int32)
+
+    verts2 = tf.constant([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]],
+                         dtype=tf.float32)
+    faces2 = tf.constant([[0, 1, 2], [0, 2, 3]], dtype=tf.int32)
+
+    verts3 = tf.constant(
+        [[0, 0, 0], [1, 1, 0], [1, -1, 0], [-1, -1, 0], [-1, 1, 0]],
+        dtype=tf.float32)
+    faces3 = tf.constant([[0, 1, 2], [0, 2, 3], [0, 3, 4], [0, 4, 1]],
+                         dtype=tf.int32)
+
+    verts4, faces4 = tf.zeros((0, 3)), tf.zeros((0, 3), dtype=tf.int32)
+
+    vertices = [verts1, verts2, verts3, verts4]
+    faces = [faces1, faces2, faces3, faces4]
+
+    meshes = Meshes(vertices, faces, batch_sizes=[b1, b2])
+
+    expected_verts_shape_padded = [b1, b2, 5, 3]
+    expected_faces_shape_padded = [b1, b2, 4, 3]
+    expected_verts_shape_flat = [sum(len(v) for v in vertices), 3]
+    expected_faces_shape_flat = [sum(len(f) for f in faces), 3]
+
+    self.assertEqual(expected_verts_shape_padded, meshes.get_padded()[0].shape)
+    self.assertEqual(expected_faces_shape_padded, meshes.get_padded()[1].shape)
+    self.assertEqual(expected_verts_shape_flat, meshes.get_flattened()[0].shape)
+    self.assertEqual(expected_faces_shape_flat, meshes.get_flattened()[1].shape)
+    self.assertEqual(b1 * b2, len(meshes.get_unpadded()[0]))
+    self.assertEqual(b1 * b2, len(meshes.get_unpadded()[1]))
+
+
+if __name__ == '__main__':
+  test_case.main()
