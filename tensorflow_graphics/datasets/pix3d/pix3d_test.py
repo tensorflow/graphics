282aef11d99f9f0dde92227a085bd930858c0d79
Robin Baumann <mail@robin-baumann.com>
2020-09-01 10:33:33 +0200

    Add Test for rendering

--- tensorflow_graphics/datasets/pix3d/pix3d_test.py
+++ tensorflow_graphics/datasets/pix3d/pix3d_test.py
@@ -19,9 +19,11 @@ from __future__ import print_function
 
 import os
 
+import tensorflow as tf
 import tensorflow_datasets.public_api as tfds
 
 from tensorflow_graphics.datasets import pix3d
+from tensorflow_graphics.rendering import triangle_rasterizer
 
 
 class Pix3dTest(tfds.testing.DatasetBuilderTestCase):
@@ -37,12 +39,53 @@ class Pix3dTest(tfds.testing.DatasetBuilderTestCase):
   MOCK_OUT_FORBIDDEN_OS_FUNCTIONS = False
   # SKIP_CHECKSUMS = True
 
+  def test_dataset_items(self):
+    builder = pix3d.Pix3d(data_dir=self.EXAMPLE_DIR)
+    self._download_and_prepare_as_dataset(builder)
+    for split_name in self.SPLITS:
+      items = tfds.as_numpy(builder.as_dataset(split=split_name))
+      for item in items:
+        expected = item['mask']
+        vertices = item['model']['vertices']
+        faces = item['model']['faces']
+        object_R = item['pose']['R']
+        object_t = item['pose']['t']
+        camera_R = item['camera']['parameters']['pose']['R']
+        camera_t = item['camera']['parameters']['pose']['t']
+        intrinsics = item['camera']['parameters']['intrinsics']
+        perspective_matrix = self._build_4x4_projection(intrinsics)
+        model_to_world = self._build_4x4_transform(object_R, object_t)
+        world_to_eye = self._build_4x4_transform(camera_R, camera_t)
+        model_to_eye = tf.matmul(world_to_eye, model_to_world)
+        rendered = triangle_rasterizer.rasterize(vertices,
+                                                 faces,
+                                                 {},
+                                                 model_to_eye,
+                                                 perspective_matrix,
+                                                 item['image'].shape[:3])
+
+        self.assertClose(expected, rendered)
+
+
   def setUp(self):  # pylint: disable=invalid-name
     super(Pix3dTest, self).setUp()
     self.builder.TRAIN_SPLIT_IDX = os.path.join(self.EXAMPLE_DIR,
                                                 'pix3d_train.npy')
     self.builder.TEST_SPLIT_IDX = os.path.join(self.EXAMPLE_DIR,
                                                'pix3d_test.npy')
+
+
+  def _build_4x4_transform(self, rotation, translation):
+    """Builds a 4x4 transform matrix."""
+    rotation4x3 = tf.concat([rotation, [[0., 0., 0.]]], axis=0)
+    translation4x1 = tf.concat([tf.transpose(translation), [[1.]]], axis=0)
+    return tf.concat([rotation4x3, translation4x1], axis=1)
+
+  def _build_4x4_projection(self, intrinsics):
+    """Builds a 4x4 perspective projection transform"""
+    intrinsics4x3 = tf.concat([intrinsics, [[0., 0., 0.]]], axis=0)
+    intrinsics4x4 = tf.concat([intrinsics4x3, [[0.], [0.], [0.], [1.]]], axis=1)
+    return intrinsics4x4
 
 
 if __name__ == '__main__':
