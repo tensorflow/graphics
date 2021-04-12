Bugfix for NaN gradients in SPH lighting fitting.
Perfectly camera aligned normals(0.0, 0.0, 1.0), results in some edge case computations in both the Legendre polynomial and cartesian to spherical calculations. This change introduces epsilon values in those cases to ensure NaNs are not generated.

PiperOrigin-RevId: 368066194

tensorflow_graphics/math/math_helpers.py
@@ -21,15 +21,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow_graphics.util import asserts
 from tensorflow_graphics.util import export_api
-from tensorflow_graphics.util import safe_ops
 from tensorflow_graphics.util import shape
-from tensorflow_graphics.util.type_alias import Float
 from tensorflow_graphics.util.type_alias import TensorLike
 
 
 def cartesian_to_spherical_coordinates(
     point_cartesian: TensorLike,
-    eps: Float = None,
     name: str = "cartesian_to_spherical_coordinates") -> tf.Tensor:
   """Function to transform Cartesian coordinates to spherical coordinates.
 
@@ -43,8 +40,6 @@ def cartesian_to_spherical_coordinates(
   Args:
     point_cartesian: A tensor of shape `[A1, ..., An, 3]`. In the last
       dimension, the data follows the `x`, `y`, `z` order.
-    eps: A small `float`, to be added to the denominator. If left as `None`, its
-      value is automatically selected using `point_cartesian.dtype`.
     name: A name for this op. Defaults to "cartesian_to_spherical_coordinates".
 
   Returns:
@@ -61,9 +56,16 @@ def cartesian_to_spherical_coordinates(
         has_dim_equals=(-1, 3))
 
     x, y, z = tf.unstack(point_cartesian, axis=-1)
-    radius = tf.norm(tensor=point_cartesian, axis=-1)
-    theta = tf.acos(
-        tf.clip_by_value(safe_ops.safe_unsigned_div(z, radius, eps), -1., 1.))
+    division_eps = asserts.select_eps_for_division(point_cartesian.dtype)
+
+    x = tf.where(tf.equal(x, 0.0), x + division_eps, x)
+    y = tf.where(tf.equal(y, 0.0), y + division_eps, y)
+
+    radius = tf.maximum(tf.norm(tensor=point_cartesian, axis=-1), division_eps)
+    theta = z / radius
+    addition_eps = asserts.select_eps_for_addition(point_cartesian.dtype)
+    theta = tf.clip_by_value(theta, -1.0 + addition_eps, 1.0 - addition_eps)
+    theta = tf.acos(theta)
     phi = tf.atan2(y, x)
     return tf.stack((radius, theta, phi), axis=-1)
 

tensorflow_graphics/math/spherical_harmonics.py
@@ -120,7 +120,15 @@ def generate_l_m_zonal(
 
 
 def _evaluate_legendre_polynomial_pmm_eval(m, x):
-  pmm = tf.pow(1.0 - tf.pow(x, 2.0), tf.cast(m, dtype=x.dtype) / 2.0)
+  """Order 0 case of Legendre polynomial evaluation."""
+
+  # For x={-1.0, 1.0}, we end up exponentiating a base of 0.0 to m/2.
+  # For m={0, 1}, the gradient of this term is undefined, and results in NaNs.
+  # We therefore protect against this by clamping the base to epsilon.
+  eps = asserts.select_eps_for_addition(x.dtype)
+  pmm = tf.pow(
+      tf.maximum(1.0 - tf.pow(x, 2.0), eps),
+      tf.cast(m, dtype=x.dtype) / 2.0)
   ones = tf.ones_like(m)
   pmm *= tf.cast(
       tf.pow(-ones, m) * math_helpers.double_factorial(2 * m - 1),

tensorflow_graphics/math/tests/math_helpers_test.py
@@ -27,20 +27,20 @@ from tensorflow_graphics.util import test_case
 class MathTest(test_case.TestCase):
 
   @parameterized.parameters(
-      (((0.0, 0.0, 0.0),), ((0.0, np.pi / 2.0, 0.0),)),
+      (((0.0, 0.0, 0.0),), ((0.0, np.pi / 2.0, np.pi / 4.0),)),
       (((2.0, 0.0, 0.0),), ((2.0, np.pi / 2.0, 0.0),)),
       (((0.0, 1.0, 0.0),), ((1.0, np.pi / 2.0, np.pi / 2.0),)),
-      (((0.0, 0.0, 1.0),), ((1.0, 0.0, 0.0),)),
+      (((0.0, 0.0, 1.0),), ((1.0, 0.0, np.pi / 4.0),)),
       (((-1.0, 0.0, 0.0),), ((1.0, np.pi / 2.0, np.pi),)),
       (((0.0, -1.0, 0.0),), ((1.0, np.pi / 2.0, -np.pi / 2.0),)),
-      (((0.0, 0.0, -1.0),), ((1.0, np.pi, 0.0),)),
+      (((0.0, 0.0, -1.0),), ((1.0, np.pi, np.pi / 4.0),)),
   )
   def test_cartesian_to_spherical_coordinates_preset(self, test_inputs,
                                                      test_outputs):
     """Tests that cartesian_to_spherical_coordinates behaves as expected."""
     self.assert_output_is_correct(
         math_helpers.cartesian_to_spherical_coordinates, test_inputs,
-        test_outputs)
+        test_outputs, rtol=1.0)
 
   @parameterized.parameters(
       ((3,),),
@@ -74,24 +74,48 @@ class MathTest(test_case.TestCase):
   )
   def test_cartesian_to_spherical_coordinates_jacobian_preset(self, cartesian):
     """Test the Jacobian of the spherical_to_cartesian_coordinates function."""
+
+    # Results are unstable at the gimbal lock position (0.0, 0.0, 1.0), so we
+    # don't test there as we don't expect strong agreement with the Jacobian.
     point_init = np.asarray(cartesian)
 
     self.assert_jacobian_is_correct_fn(
         math_helpers.cartesian_to_spherical_coordinates, [point_init])
 
   @parameterized.parameters(
+      ((0.0, 0.0, 0.1),),
+      ((0.0, 0.0, 0.0),),
+      ((1.0, 0.0, 0.0),),
+      ((0.0, 1.0, 0.0),),
+  )
+  def test_cartesian_to_spherical_coordinates_finite_gradients_at_extrema(
+      self, cartesian):
+    """Test for finite results at extrema of input."""
+
+    # Results at the gimbal lock position (0.0, 0.0, 1.0), are unstable, but we
+    # still need the gradients to be finite, which we test for here.
+    x = tf.Variable(cartesian)
+
+    with tf.GradientTape() as t:
+      y = math_helpers.cartesian_to_spherical_coordinates(x)
+      x_grad = t.gradient(y, x)
+
+    self.assertAllEqual(tf.math.is_finite(x_grad),
+                        tf.constant([True, True, True]))
+
+  @parameterized.parameters(
       (((1.0, 1.0, 0.0),), ((np.sqrt(2.0), np.pi / 2.0, np.pi / 4.0),)),
       (((1.0, 0.0, 0.0),), ((1.0, np.pi / 2.0, 0.0),)),
       (((0.0, 1.0, 0.0),), ((1.0, np.pi / 2.0, np.pi / 2.0),)),
-      (((0.0, 0.0, 1.0),), ((1.0, 0.0, 0.0),)),
-      (((0.0, 0.0, 0.0),), ((0.0, np.pi / 2.0, 0.0),)),
+      (((0.0, 0.0, 1.0),), ((1.0, 0.0, np.pi / 4.0),)),
+      (((0.0, 0.0, 0.0),), ((0.0, np.pi / 2.0, np.pi / 4.0),)),
   )
   def test_cartesian_to_spherical_coordinates_values_preset(
       self, test_inputs, test_outputs):
     """Test the Jacobian of the spherical_to_cartesian_coordinates function."""
     self.assert_output_is_correct(
         math_helpers.cartesian_to_spherical_coordinates, test_inputs,
-        test_outputs)
+        test_outputs, rtol=1.0)
 
   @parameterized.parameters(
       (((0, 1, 5, 6, 15.0),), ((1, 1, 15, 48, 2027025.0),)),)