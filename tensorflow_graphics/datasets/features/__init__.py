dc688f1d61e5f58a9bd0a8659655827f1fe3eec2
Yilei Yang <yileiyang@google.com>
2022-03-24 09:46:45 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 437017139

--- tensorflow_graphics/datasets/features/__init__.py
+++ tensorflow_graphics/datasets/features/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Lint as: python3
 """`tensorflow_graphics.datasets.features` API defining feature types."""
 
 from tensorflow_graphics.datasets.features.camera_feature import Camera
--- tensorflow_graphics/projects/local_implicit_grid/core/postprocess.py
+++ tensorflow_graphics/projects/local_implicit_grid/core/postprocess.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Lint as: python3
 """Postprocess to remove interior backface from reconstruction artifact.
 """
 import numpy as np
